b7d04cfdcc2c2625a35b80c98f5128832a28e5f6
Tony ElHabr <anthonyelhabr@gmail.com>
2020-08-17 17:34:09 -0500

    pretty good working for 5

--- scripts/05_nba_3fg_stability.R
+++ scripts/05_nba_3fg_stability.R
@@ -7,17 +7,17 @@
 extrafont::loadfonts(device = 'win', quiet = TRUE)
 library(tidyverse)
 require(here)
-require(readxl)
-require(ggtext)
+library(patchwork)
+library(magick)
 
 theme_set(theme_minimal())
 theme_update(
-  text = element_text(family = 'Fira Mono'),
+  text = element_text(family = 'Karla'),
   title = element_text('Karla', size = 14, color = 'gray20'),
   plot.title = element_text('Karla', face = 'bold', size = 18, color = 'gray20'),
   plot.title.position = 'plot',
   plot.subtitle = element_text('Karla', face = 'bold', size = 14, color = 'gray50'),
-  axis.text = element_text(size = 14),
+  axis.text = element_text('Karla', size = 14),
   # axis.title = element_text(size = 24, face = 'bold'),
   axis.title = element_text(size = 14, face = 'bold', hjust = 0.99),
   # axis.text = element_text('Karla', size = 12, face = 'bold', color = 'gray20'),
@@ -44,7 +44,7 @@ theme_update(
   # legend.position = c(.85, .85))
   panel.background = element_rect(fill = '#fffaf0', color = NA)
 )
-update_geom_defaults('text', list(family = 'Fira Mono', size = 3.5))
+update_geom_defaults('text', list(family = 'Karla', size = 3.5))
 
 # path <- 'https://raw.githubusercontent.com/wh0801/NBA-shooting-rationality-2016-17-Regular-Season/master/Project-Team%20shoot%20rationality%20-%20github/Shot%20data/shot%20log%20ATL.csv'
 # shots_atl <- path %>% read_csv() %>% janitor::clean_names()
@@ -86,10 +86,10 @@ shots_2013_2014$player_name
 fix_player_names <- function(x) {
   str_replace_all(
     x,
-    c('nowtizski' = 'nowitzki',
-      'dwyane wade' = 'dwayne wade',
+    c(
+      'nowtizski' = 'nowitzki',
+      'dwyane wade' = 'dwayne wade', # doesn't take a lot of 3s, but whatever
       'mnta ellis' = 'monta ellis',
-      # 'o.j. mayo' = 'oj mayo',
       'tim hardaway jr' = 'time hardaway jr',
       'danilo gallinari' = 'danilo gallinai'
     )
@@ -195,7 +195,7 @@ do_kr20_fgm <- partial(do_f_at, shots_mini_filt, f = kr20, col = 'fgm', ... = )
 
 k_max <- shots_mini_filt %>% filter(idx == max(idx)) %>% pull(idx)
 k_max
-k_seq <- seq.int(10, k_max, by = 10)
+k_seq <- seq.int(10L, k_max, by = 10L)
 krs_fgm <-
   tibble(k = k_seq) %>% 
   mutate(
@@ -212,32 +212,34 @@ cutoff_fgm <-
   filter(value_2 >= 0.5) %>% 
   filter(k != 1L) %>% 
   slice(1) %>% 
-  pull(k)
+  pull(k) %>% 
+  as.integer()
 cutoff_fgm
 
-fit_lm <-
-  krs_fgm %>% 
-  lm(value_2 ~ k, data = .)
-fit_lm
-
-# fit_lm_sqrt <-
+# fit_lm <-
 #   krs_fgm %>% 
-#   lm(value ~ k^2, data = .)
-# fit_lm_sqrt
-
-pred_lm <-
-  fit_lm_sqrt %>% 
-  broom::augment(new_data = tibble(k = k_seq)) %>% 
-  # rename(.pred = value_2) %>% 
-  full_join(krs_fgm)
-pred_lm
-
-# pred_lm %>% 
-#   ggplot() +
-#   aes(x = k) +
-#   geom_line(aes(y = .fitted), color = 'blue') +
-#   geom_step(aes(y = value_2), color = 'black')
-
+#   lm(value_2 ~ k, data = .)
+# fit_lm
+# 
+# # fit_lm_sqrt <-
+# #   krs_fgm %>% 
+# #   lm(value ~ k^2, data = .)
+# # fit_lm_sqrt
+# 
+# pred_lm <-
+#   fit_lm %>% 
+#   broom::augment(new_data = tibble(k = k_seq)) %>% 
+#   full_join(krs_fgm)
+# pred_lm
+# 
+# cutoff_fgm_pred <-
+#   pred_lm %>% 
+#   filter(.fitted >= 0.5) %>% 
+#   filter(k != 1L) %>% 
+#   slice(1) %>% 
+#   pull(k) %>% 
+#   as.integer()
+# cutoff_fgm_pred
 
 shots_n_rnk <- 
   shots_mini_filt %>% 
@@ -248,34 +250,169 @@ shots_n_rnk <-
   arrange(rnk)
 shots_n_rnk
 
-players_top <- shots_n_rnk %>% filter(rnk <= 10L)
+players_filt <- c('stephen curry', 'damian lillard', 'klay thompson', 'james harden','kyle lowry', 'kyle korver')
+players_top <- 
+  shots_n_rnk %>% 
+  # filter(rnk <= 10L) %>% 
+  filter(player %in% players_filt) %>% 
+  mutate(player_pretty = player %>% str_to_title())
 players_top
+
+# players_top %>% pull(player) %>% clipr::write_clip()
+# teamcolors::league_pal('nba')
+players_colors <-
+  tibble(
+    player = c('stephen curry', 'damian lillard', 'klay thompson', 'james harden', 'trevor ariza', 'wesley matthews', 'gerald green', 'kyle lowry', 'kevin love', 'kyle korver'),
+    color = c('#fdb927', '#000000', '#fdb927', '#ce1141', '#ce1141', '#000000', '#e56020', '#ce1141', '#860038', '#e13a3e')
+  ) %>% 
+  filter(player %in% players_filt) %>% 
+  deframe()
+players_colors %>% scales::show_col()
+
 shots_viz <-
   shots_mini_filt %>% 
   semi_join(shots_n_rnk %>% filter(rnk <= 100)) %>% 
   mutate(is_tenth = idx %% 10 == 0) %>% 
-  filter(is_tenth)
+  filter(is_tenth) %>% 
+  select(-is_tenth)
+shots_viz
 
+shots_n_rnk %>% filter(rnk == 100)
+
+shots_viz_top <-
+  shots_viz %>% 
+  inner_join(players_top) %>% 
+  mutate(lab = sprintf('%s (%.1f%%)', player_pretty, round(100 * fgm_rate_cumu)))
+shots_viz_top
+
+x_pos_lab <- 1800L
+height <- 750
+animate_partial <-
+  partial(
+    gganimate::animate,
+    nframe = 150,
+    end_pause = 50,
+    fps = 20,
+    width = 600,
+    ... = 
+  )
+
+add_common_layers <- function(...) {
+  list(
+    geom_vline(
+      data = tibble(idx = cutoff_fgm),
+      color = 'grey20',
+      linetype = 1,
+      aes(xintercept = idx, group = NULL),
+      size = 1.1
+    ),
+    scale_x_continuous(labels = scales::comma_format(accuracy = 1), limits = c(0, 2200)),
+    scale_y_continuous(labels = scales::percent_format(accuracy = 1)),
+    theme(
+      panel.grid.major.y = element_blank()
+    )
+  )
+}
+
+# Reference: https://github.com/thomasp85/gganimate/wiki/Temperature-time-series
 viz_fgm_rate_cumu <-
   shots_viz %>% 
   ggplot() +
   aes(x = idx, y = fgm_rate_cumu, group = player) +
+  add_common_layers() +
   geom_step(alpha = 0.1) +
-  # geom_step(
-  #   data = shots_agg_all %>% filter(player %in% players_top),
-  #   aes(color = player),
-  #   show.legend = FALSE
-  # ) +
-  # gganimate::transition_reveal(along = idx) +
-  # geom_vline(
-  #   # data = tibble(x = 60),
-  #   # xintercept = 60,
-  #   data = tibble(idx = rep(threshold_overperform_young, 200 - 60)),
-  #   aes(xintercept = idx)
-  # ) +
-  theme_minimal() +
+  geom_step(
+    data = shots_viz_top,
+    aes(color = player),
+    size = 1.1,
+    show.legend = FALSE
+  ) +
+  geom_segment(
+    data = shots_viz_top,
+    aes(x = idx, y = fgm_rate_cumu, xend = x_pos_lab, yend = fgm_rate_cumu),
+    linetype = 2,
+    size = 1
+  ) +
+  geom_text(
+    # Shift curry up to avoid conflict with Klay near the end.
+    data = 
+      shots_viz_top %>% 
+      mutate(y = case_when(player == 'stephen curry' ~ fgm_rate_cumu + 0.005, TRUE ~ fgm_rate_cumu)),
+    # data = shots_viz_top,
+    aes(x = x_pos_lab + 1L, y = y, label = lab),
+    # family = 'Karla',
+    hjust = 0
+  ) +
+  scale_color_manual(values = players_colors) +
+  geom_text(
+    data = tibble(idx = cutoff_fgm),
+    aes(x = idx, y = 0.55, label = glue::glue('Stability point'), group = NULL),
+    size = 4,
+    hjust = 1.05,
+    color = 'grey20'
+  ) +
+  gganimate::transition_reveal(along = idx) +
+  coord_cartesian(ylim = c(0.2, 0.6), clip = 'off') +
+  theme(
+    axis.text.x = element_blank()
+  ) +
   labs(
-    x = 'Shot #',
+    x = NULL,
     y = 'Cumualitve 3FG% Average'
   )
 viz_fgm_rate_cumu
+
+viz_fgm_rate_cumu_gif <- animate_partial(viz_fgm_rate_cumu, height = 0.75 * height)
+viz_fgm_rate_cumu_gif
+
+krs_viz <- krs_fgm %>% rename(idx = k)
+viz_krs <-
+  krs_viz %>% 
+  # Last value_2 values are NAs because n = 1, so add dummy values to make the plot line up with the prior plot.
+  fill(value_2) %>% 
+  ggplot() +
+  aes(x = idx, y = value_2) +
+  add_common_layers() +
+  geom_segment(
+    data = krs_viz %>% mutate(x = 0, y = value_2, yend = value_2) %>% filter(idx <= cutoff_fgm),
+    aes(x = x, y = y, xend = idx, yend = yend),
+    linetype = 2,
+    size = 1.1,
+    color = 'grey20'
+  ) +
+  geom_step() +
+  gganimate::transition_reveal(along = idx) +
+  # # Add this like a title. Shouldn't be animated.
+  # annotate(
+  #   geom = 'text',
+  #   x = 20,
+  #   y = 0.9,
+  #   size = 5,
+  #   hjust = 0,
+  #   fontface = 'bold',
+  #   family = 'Karla',
+  #   color = 'grey20',
+  #   label = glue::glue('Stability Rate')
+  # ) +
+  labs(
+    y = 'Stability Rate',
+    x = 'Shot #'
+  )
+viz_krs
+
+viz_krs_gif <- animate_partial(viz_krs, height = 0.25 * height)
+viz_krs_gif
+# viz_patched <- (viz_fgm_rate_cumu) + (viz_krs + plot_layout(ncol = 1, heights = c(3, 1))
+# viz_patched
+
+# https://github.com/thomasp85/gganimate/wiki/Animation-Composition
+viz_fgm_rate_cumu_mgif <- magick::image_read(viz_fgm_rate_cumu_gif)
+viz_krs_mgif <- magick::image_read(viz_krs_gif)
+
+res_gif <- magick::image_append(c(viz_fgm_rate_cumu_mgif[1], viz_krs_mgif[1]), stack = TRUE)
+for(i in 2:100){
+  combo_gif <- magick::image_append(c(viz_fgm_rate_cumu_mgif[i], viz_krs_mgif[i]), stack = TRUE)
+  res_gif <- c(res_gif, combo_gif)
+}
+res_gif
+
