76dff6cd7b57540d65f28e41d86831d0531f8841
Tony ElHabr <anthonyelhabr@gmail.com>
2021-03-28 18:02:53 -0500

    init epl xg by game state stuff

---
+++ 20-202021_epl_xg_stats/0x-gamestate_xg.R
@@ -0,0 +1,387 @@
+
+library(tidyverse)
+extrafont::loadfonts(device = 'win', quiet = TRUE)
+theme_set(theme_minimal())
+theme_update(
+  text = element_text(family = 'Karla'),
+  title = element_text('Karla', size = 14, color = 'gray20'),
+  plot.title = element_text('Karla', face = 'bold', size = 18, color = 'gray20', hjust = 0.5),
+  # plot.title.position = 'plot',
+  plot.subtitle = element_text('Karla', face = 'bold', size = 14, color = 'gray50'),
+  axis.text = element_text('Karla', size = 14),
+  # axis.title = element_text(size = 24, face = 'bold'),
+  axis.title = element_text(size = 14, face = 'bold', hjust = 0.99),
+  # axis.text = element_text('Karla', size = 12, face = 'bold', color = 'gray20'),
+  # axis.title.x = element_text(hjust = 0.95),
+  # axis.title.y = element_text(hjust = 0.95),
+  # axis.line = element_line(color = 'gray80'),
+  axis.line = element_blank(),
+  # panel.grid.major = element_line(color = 'gray80'),
+  # panel.grid.minor = element_line(color = 'gray80'),
+  panel.grid.major = element_blank(),
+  # panel.grid.minor.x = element_blank(),
+  # panel.grid.minor.y = element_blank(),
+  panel.grid.minor = element_blank(),
+  plot.margin = margin(10, 10, 10, 10),
+  plot.background = element_rect(fill = '#ffffff', color = NA),
+  plot.caption = element_text('Karla', size = 14, color = 'gray20', hjust = 1),
+  plot.caption.position = 'plot',
+  plot.tag = ggtext::element_markdown('Karla', size = 12, color = 'gray20', hjust = 0), 
+  plot.tag.position = c(.01, 0.01),
+  legend.text = element_text(size = 14),
+  # legend.background = element_rect(fill = '#F3F4F6', color = '#F3F4F6'),
+  # legend.position = c(.85, .85))
+  # strip.text = element_text(size = 18),
+  strip.background = element_blank(),
+  panel.background = element_rect(fill = '#ffffff', color = NA)
+)
+update_geom_defaults('text', list(family = 'Karla', size = 4))
+
+dir_proj <- '999-dev'
+season <- 2019
+path_matches <- here::here(dir_proj, sprintf('matches_%s.rds', season))
+path_shots <- here::here(dir_proj, sprintf('shots_%s.rds', season))
+overwrite <- FALSE
+# Reference: https://gist.github.com/Torvaney/42cd82addb3ba2c4f33ec3247e66889c
+if(!file.exists(path_shots) & overwrite) {
+  if(!file.exists(path_matches) & overwrite) {
+    extract_json <- function(html, varname) {
+      # Extract a JSON variable from the raw html
+      html %>% 
+        rvest::html_nodes('script') %>% 
+        rvest::html_text(trim = TRUE) %>% 
+        keep(str_detect, varname) %>% 
+        str_extract("(?<=JSON.parse\\(').*(?='\\))") %>% 
+        stringi::stri_unescape_unicode() %>% 
+        jsonlite::fromJSON()
+    }
+    
+    unnest_df <- function(df, name) {
+      # Hack to convert list/nested dfs to a single df while keeping sane column 
+      # names
+      if (!is.data.frame(df)) {
+        return(tibble(!!name := df))
+      }
+      
+      tbl <- as_tibble(df)
+      colnames(tbl) <- str_c(name, '_', colnames(tbl))
+      
+      tbl
+    }
+    
+    fetch_matches <- function(league = 'EPL', season = 2020) {
+      # Fetch a dataframe of matches for an individual league/season's page
+      # league_url %>% 
+      sprintf('https://understat.com/league/%s/%s', toupper(league), season) %>% 
+        xml2::read_html() %>% 
+        extract_json('datesData') %>% 
+        imap_dfc(unnest_df) %>% 
+        rename(match_id = id) %>% 
+        type_convert()
+    }
+    
+    matches <- fetch_matches(season = season)
+    write_rds(matches, path_matches)
+  } else {
+    matches <- path_matches %>% read_rds()
+  }
+  shots <-
+    matches %>%
+    filter(isResult) %>% 
+    pull(match_id) %>% 
+    map_dfr(slowly(possibly(understatr::get_match_shots, tibble())))
+  
+  write_rds(shots, path_shots)
+} else {
+  shots <- path_shots %>% read_rds()
+}
+
+team_stats <- understatr::get_league_teams_stats('EPL', year = season)
+standings <-
+  team_stats %>% 
+  group_by(team = team_name) %>% 
+  summarize(
+    across(pts, sum)
+  ) %>% 
+  ungroup() %>%
+  arrange(rnk = row_number(desc(pts)))
+standings
+
+shots_slim <-
+  shots %>% 
+  mutate(
+    across(c(match_id, minute), as.integer),
+    penalty = if_else(situation == 'Penalty', TRUE, FALSE)
+  ) %>% 
+  arrange(date, match_id, minute) %>% 
+  select(
+    season, date, match_id, minute, team_h = h_team, team_a = a_team, side = h_a, xg = xG, penalty, result # , g_h = h_goals, g_a = a_goals
+  )
+shots_slim
+
+xg_penalty <- shots_slim %>% filter(penalty) %>% pull(xg) %>% mean()
+xg_penalty
+rgx_g <- '^(np)?[x]?g'
+
+# Pad such that minutes after last event for games with last shot before 90 min (e.g. last shot at 87th minute) gets counted.
+shots_redux_init <-
+  bind_rows(
+    shots_slim %>% 
+      filter(side == 'h') %>% 
+      rename(xg_h = xg) %>% 
+      mutate(g_h = if_else(result == 'Goal', 1L, 0L)),
+    shots_slim %>% 
+      filter(side == 'a') %>% 
+      rename(xg_a = xg) %>% 
+      mutate(g_a = if_else(result == 'Goal', 1L, 0L))
+  ) %>% 
+  arrange(season, date, match_id, minute) %>% 
+  mutate(
+    npxg_h = ifelse(penalty & side == 'h', NA_real_, xg_h),
+    npxg_a = ifelse(penalty & side == 'a', NA_real_, xg_a)
+  )
+shots_redux_init
+
+shots_redux_pad <-
+  shots_redux_init %>% 
+  group_by(season, date, match_id) %>% 
+  slice_max(minute, with_ties = FALSE) %>% 
+  ungroup() %>% 
+  filter(minute < 90) %>% 
+  mutate(result = 'dummy') %>% 
+  select(season, date, match_id, team_h, team_a, side, result)
+
+shots_redux <-
+  bind_rows(shots_redux_init, shots_redux_pad) %>% 
+  arrange(season, date, match_id, minute) %>% 
+  group_by(season, date, match_id) %>% 
+  mutate(
+    dur = minute - dplyr::lag(minute, default = 0L),
+    across(matches(sprintf('%s_', rgx_g)), list(cumu = ~cumsum(coalesce(.x, 0))))
+  ) %>% 
+  # ungroup() %>% 
+  # mutate(
+  #   g_state_h = g_h_cumu - g_a_cumu,
+  #   g_state_a = g_a_cumu - g_h_cumu
+  # ) %>% 
+  # # filter(result != 'Goal') %>% 
+  mutate(
+    g_state_h = dplyr::lag(g_h_cumu, 1) - dplyr::lag(g_a_cumu, 1),
+    g_state_a = dplyr::lag(g_a_cumu, 1) - dplyr::lag(g_h_cumu, 1)
+  ) %>%
+  ungroup() %>%
+  mutate(
+    # The coalesce is for the first event in a match.
+    across(matches('^g_'), ~coalesce(.x, 0) %>% as.integer())
+  )
+shots_redux %>% filter(result == 'Goal' | lead(result) == 'Goal')
+shots_redux %>% filter(result == 'dummy')
+shots_redux %>% filter(penalty)
+
+.f_rename <- function(side) {
+  suffix <- sprintf('_%s$', side)
+  side_opp <- ifelse(side == 'h', 'a', 'h')
+  suffix_opp <- sprintf('_%s$', side_opp)
+  shots_redux %>% 
+    select(-matches('_cumu$')) %>% 
+    rename_with(~str_replace(.x, suffix, ''), c(matches(suffix))) %>% 
+    rename_with(~str_replace(.x, suffix_opp, '_opp'), c(matches(suffix_opp))) %>% 
+    mutate(side = !!side)
+}
+
+shots_by_team <- bind_rows(.f_rename('h'), .f_rename('a'))
+shots_by_team
+
+team_mapping <- 
+  xengagement::team_accounts_mapping %>% 
+  filter(league %in% c('epl', 'champ'))
+team_mapping
+# team_mapping %>% filter(team %>% str_detect('Wolves'))
+
+states <-
+  shots_by_team %>% 
+  select(-g_state_opp) %>% 
+  # mutate(
+  #   across(g_state, ~case_when(.x <= -3L ~ '<=-3', .x >= 3L ~ '>=+3', .x > 0 ~ sprintf('+%d', .x), TRUE ~ as.character(.x)))
+  # ) %>%
+  # mutate(
+  #   across(g_state, ~factor(.x, levels = c('<=-3', -2:0, sprintf('+%d', 1:2), '>=+3')))
+  # ) %>%
+  # mutate(
+  #   across(g_state, ~case_when(.x < 0L ~ '<0', .x > 0L ~ '>0', TRUE ~ '0'))
+  # ) %>% 
+  # mutate(
+  #   across(g_state, ~factor(.x, levels = c('<0', '0', '>0')))
+# ) %>% 
+filter(g_state <= 2L & g_state >= -2L) %>%
+  mutate(
+    across(g_state, ~case_when(.x > 0 ~ sprintf('+%d', .x), TRUE ~ as.character(.x)))
+  ) %>%
+  mutate(
+    across(g_state, ~factor(.x, levels = c(-2:0, sprintf('+%d', 1:2))))
+  ) %>% 
+  # Just to have something at the end that I don't comment out when testing.
+  arrange(season, date, match_id, minute, team)
+states
+
+states_by_team <-
+  states %>% 
+  group_by(season, team, g_state) %>% 
+  summarize(
+    shots = sum(!is.na(xg)),
+    shots_opp = sum(!is.na(xg_opp)),
+    across(c(dur, matches(rgx_g)), ~sum(.x, na.rm = TRUE))
+  ) %>% 
+  ungroup() %>% 
+  mutate(
+    npxg_ps = npxg / shots,
+    npxg_opp_ps = npxg_opp / shots_opp
+  ) %>% 
+  mutate(
+    across(c(matches(rgx_g), -g_state, matches('^shots')), list(p90 = ~90 * .x / dur))
+  ) %>% 
+  mutate(
+    shots_p90_diff = shots_p90 - shots_opp_p90,
+    npxg_p90_diff = npxg_p90 - npxg_opp_p90,
+    npxg_ps_diff = npxg_ps - npxg_opp_ps
+  ) %>% 
+  left_join(
+    team_mapping %>% 
+      select(team_new = team, team = team_understat)
+  ) %>% 
+  select(-team) %>% 
+  rename(team = team_new) %>% 
+  mutate(
+    # across(
+    #   npxg_p90_diff,
+    #   ~case_when(.x <= -3 ~ -3, .x >= 3 ~ 3, TRUE ~ .x)
+    # ),
+    across(team, ~factor(.x, levels = rev(team_mapping$team)))
+  ) %>% 
+  relocate(season, team)
+states_by_team
+
+states_by_team_agg <-
+  states_by_team %>% 
+  group_by(season, g_state) %>% 
+  # mutate(dur_total = sum(dur)) %>% 
+  summarize(
+    cnt = n(),
+    dur_total = sum(dur),
+    across(matches('_diff$'), list(mean = ~sum(dur * .x, na.rm = TRUE) / dur_total))
+  ) %>% 
+  ungroup()
+states_by_team_agg %>% mutate(across(matches('_mean'), round, 2))
+states_by_team_agg$npxg_p90_diff_mean
+
+states_by_team_oe <-
+  states_by_team %>% 
+  left_join(
+    states_by_team_agg
+  ) %>% 
+  mutate(
+    shots_p90_oe = shots_p90_diff - shots_p90_diff_mean,
+    npxg_p90_oe = npxg_p90_diff - npxg_p90_diff_mean
+  ) %>% 
+  group_by(season, g_state) %>% 
+  mutate(
+    across(matches('_oe$'), list(rnk = ~row_number(desc(.x)), rnk_inv = ~row_number(.x)))
+  ) %>% 
+  ungroup() %>% 
+  filter(shots_p90_oe_rnk == 1L | npxg_p90_oe_rnk == 1L | shots_p90_oe_rnk_inv == 1L | npxg_p90_oe_rnk_inv == 1L) %>% 
+  arrange(season, g_state)
+
+states_by_team_oe %>% 
+  filter(g_state == '-1') %>% 
+  glimpse()
+
+# states_by_team %>% 
+#   filter(g_state == '<=-3') %>% 
+#   arrange(desc(npxg_p90_diff))
+# 
+# states %>% 
+#   filter(team %>% str_detect('Tottenham')) %>% 
+#   filter(g_state == '<=-3') %>% 
+#   summarize(across(c(dur, xg, xg_opp), sum, na.rm = TRUE)) %>% 
+#   mutate(diff = 90 * (xg - xg_opp) / dur)
+
+.plot_heatmap <- function(col, ...) {
+  states_by_team %>% 
+    filter(g_state %in% c('-1', '0', '+1')) %>% 
+    ggplot() +
+    aes(y = team, x = g_state) +
+    geom_tile(aes(fill = {{ col }})) +
+    # scale_fill_viridis_c() +
+    # scale_fill_distiller() +
+    # theme_minimal() +
+    labs(
+      # x = 'Game State',
+      y = NULL
+    ) +
+    theme(
+      # plot.title.position = 'plot',
+      # title = element_text(hjust = 0.5),
+      panel.grid.major = element_blank(),
+      legend.position = 'top'
+    )
+}
+
+viz_npxg_p90_diff <-
+  .plot_heatmap(npxg_p90_diff) +
+  # guides(fill = guide_legend(title = 'npxGD/90', nrow = 1)) +
+  guides(fill = guide_legend(title = 'Non-Penalty xG Differential Per 90 Min.', nrow = 1)) +
+  # scale_fill_distiller(palette = 'PRGn', direction = 1, na.value = 'grey50') +
+  scale_fill_gradient2(
+    midpoint = 0,
+    low = '#e9a3c9',
+    mid = '#f7f7f7',
+    high = '#a1d76a'
+  ) +
+  labs(
+    title = 'Better teams have better',
+    tag = '**Viz**: Tony ElHabr | **Data**: understat'
+  )
+viz_npxg_p90_diff
+
+viz_npxg_ps_diff <-
+  .plot_heatmap(npxg_ps_diff) +
+  # guides(fill = guide_legend(title = 'npxGD/shot', nrow = 1)) +
+  guides(fill = guide_legend(title = 'Non-Penalty xG Differential Per Shot', nrow = 1)) +
+  scale_fill_gradient2(
+    midpoint = 0,
+    low = '#5ab4ac',
+    mid = '#f5f5f5',
+    high = '#d8b365'
+  ) +
+  theme(
+    axis.text.y = element_blank()
+  ) +
+  # labs(title = 'Non-Penalty xG Differential Per Shot')
+  labs(
+    title = 'Teams shoot less efficiently when they\'re down' # ,
+    # subtitle = 'Non-Penalty xG Differential Per Shot'
+  )
+viz_npxg_ps_diff
+
+viz_shots_p90_diff <-
+  .plot_heatmap(shots_p90_diff) +
+  # guides(fill = guide_legend(title = 'shots/90', nrow = 1)) +
+  guides(fill = guide_legend(title = 'Shot Differential Per 90 Min.', nrow = 1)) +
+  geom_text(
+    family = 'Fira Code',
+    size = 4,
+    aes(label = scales::number(shots_p90_diff, accuracy = 0.1))
+  ) +
+  scale_fill_gradient2(
+    midpoint = 0,
+    low = '#67a9cf',
+    mid = '#f7f7f7',
+    high = '#ef8a62'
+  ) +
+  # labs(title = 'Shot Differential Per 90 Min.')
+  labs(
+    x = 'Game State',
+    # caption = 'Game states with goal differentials less/greater than +-2 not shown.',
+    title = '...because they shoot more frequently (and desperately).'
+  )
+viz_shots_p90_diff
