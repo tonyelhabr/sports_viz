305f26f93bfbecc7f0fc7488297df242fa46b5cd
Tony ElHabr <anthonyelhabr@gmail.com>
2021-04-26 19:07:52 -0500

    progress on 25

--- 25-202021_soccer_refs/01-scrape_ref_stats.R
+++ 25-202021_soccer_refs/01-scrape_ref_stats.R
@@ -1,66 +1,61 @@
 
 library(tidyverse)
 dir_proj <- '25-202021_soccer_refs'
-overwrite <- FALSE
 dir_data <- file.path(dir_proj, 'data')
 fs::dir_create(dir_data)
-path_stats <- file.path(dir_data, 'stats.rds')
+path_results <- file.path(dir_proj, 'results.rds')
+path_stats <- file.path(dir_proj, 'stats.rds')
 
 params <-
   crossing(
-    country = c('ENG', 'ITA', 'GER', 'FRA', 'SPA'),
+    country = c('ENG', 'ITA', 'GER', 'FRA', 'ESP'),
     gender = c('M'),
     season_end_year = c(2020:2021),
     tier = '1st'
   )
 params
 
-if(!file.exists(path_stats) & overwrite) {
+urls <-
+  params %>% 
+  mutate(url = pmap(list(country, gender, season_end_year, tier), worldfootballR::get_match_urls))%>% 
+  unnest(url)
+urls
 
-  urls <-
-    params %>% 
-    mutate(url = pmap(list(country, gender, season_end_year, tier), worldfootballR::get_match_urls))%>% 
-    unnest(url)
-  urls
+f <- possibly(worldfootballR::get_match_results, otherwise = tibble())
+results_nest <- 
+  params %>% 
+  mutate(data = pmap(list(country, gender, season_end_year, tier), f))
+beepr::beep(3)
 
-  f <- possibly(worldfootballR::get_match_results, otherwise = tibble())
-  results_nest <- 
-    params %>% 
-    mutate(data = pmap(list(country, gender, season_end_year, tier), f))
-  # beepr::beep(3)
-  
-  results <-
-    results_nest %>% 
-    unnest(data) %>% 
-    as_tibble() %>% 
-    janitor::clean_names()
-  results
-  
-  scrape_misc_stats <- function(url, stat_type = 'misc', team_or_player = 'player', overwrite = FALSE) {
-    path <- file.path(dir_data, sprintf('%s-%s-%s.rds', basename(url), stat_type, team_or_player))
-    if(file.exists(path) & !overwrite) {
-      cat(glue::glue('{Sys.time()}: Returning early for `url = {url}`'), sep = '\n')
-      return(read_rds(path))
-    }
-    # browser()
-    res <- worldfootballR::get_advanced_match_stats(url, stat_type = stat_type, team_or_player = team_or_player)
-    cat(glue::glue('{Sys.time()}: Retrieved data for `url = {url}`'), sep = '\n')
-    write_rds(res, path)
-    res
+results <-
+  results_nest %>% 
+  unnest(data) %>% 
+  janitor::clean_names() %>% 
+  select(-matches('_2$'))
+results
+write_rds(results, path_results)
+
+scrape_misc_stats <- function(url, stat_type = 'misc', team_or_player = 'player', overwrite = FALSE) {
+  path <- file.path(dir_data, sprintf('%s-%s-%s.rds', basename(url), stat_type, team_or_player))
+  suffix <- glue::glue('for `url = {url}`, `stat_type = "{stat_type}"`, `team_or_player = "{team_or_player}"`.')
+  if(file.exists(path) & !overwrite) {
+    cat(glue::glue('{Sys.time()}: Returning early {suffix}'), sep = '\n')
+    return(read_rds(path))
   }
-  
-  g <- possibly(scrape_misc_stats, otherwise = tibble())
-  stats <-
-    urls %>% 
-    mutate(data = map(url, g)) %>% 
-    unnest(data) %>% 
-    as_tibble() %>% 
-    janitor::clean_names()
-  beepr::beep(3)
-  stats
-  write_rds(stats, path_stats)
-  
-} else {
-  stats <- path_stats %>% read_rds()
+  res <- worldfootballR::get_advanced_match_stats(url, stat_type = stat_type, team_or_player = team_or_player)
+  cat(glue::glue('{Sys.time()}: Retrieved data {suffix}'), sep = '\n')
+  write_rds(res, path)
+  res
 }
+
+g <- possibly(scrape_misc_stats, otherwise = tibble())
+stats <-
+  urls %>% 
+  mutate(data = map(url, g)) %>% 
+  unnest(data) %>% 
+  as_tibble() %>% 
+  janitor::clean_names()
+stats
+beepr::beep(3)
+write_rds(stats, path_stats)
 
--- 25-202021_soccer_refs/02-process_ref_stats.R
+++ 25-202021_soccer_refs/02-process_ref_stats.R
@@ -1,8 +1,9 @@
 
 library(tidyverse)
 dir_proj <- '25-202021_soccer_refs'
-dir_data <- file.path(dir_proj, 'data')
-path_stats <- file.path(dir_data, 'stats.rds')
+path_results <- file.path(dir_proj, 'results.rds')
+path_stats <- file.path(dir_proj, 'stats.rds')
+results <- path_results %>% read_rds()
 stats <- path_stats %>% read_rds()
 
 # teams_stats <- stats %>% count(team = home_team, name = 'n_stats')
@@ -19,6 +20,10 @@ team_corrections <-
     team_stats = c('Brighton & Hove Albion', 'Huddersfield Town', 'Manchester United', 'Newcastle United', 'Sheffield United', 'Tottenham Hotspur', 'West Bromwich Albion', 'West Ham United', 'Wolverhampton Wanderers'),
     team_results = c('Brighton', 'Huddersfield', 'Manchester Utd', 'Newcastle Utd', 'Sheffield Utd', 'Tottenham', 'West Brom', 'West Ham', 'Wolves')
   )
+
+.add_ratio_foul_tackle_col <- function(data) {
+  data %>% mutate(ratio_foul_tackle = n_foul_committed / n_tackle_won)
+}
 
 misc_w_refs <-
   stats %>% 
@@ -37,7 +42,7 @@ misc_w_refs <-
     across(team_away, ~coalesce(team_away_alt, .x))
   ) %>% 
   select(
-    idx_stats, season, team_home, team_away, home_x_g, away_x_g, date, team, player,
+    idx_stats, country, tier, gender, season, team_home, team_away, home_x_g, away_x_g, date, team, player,
     mp = min,
     card_y = crd_y,
     card_r = crd_r,
@@ -49,15 +54,21 @@ misc_w_refs <-
     pk_won = p_kwon,
     pk_conceded = p_kcon
   ) %>% 
+  select(-pk_won) %>% 
+  rename()
   # filter(idx_stats == 10204L) %>% 
   inner_join(
     results %>%
       mutate(idx_results = row_number()) %>% 
       select(idx_results, wk, date, team_home = home, team_away = away, referee)
   ) %>% 
+  .add_ratio_foul_tackle_col() %>% 
   select(
     idx_stats,
     idx_results,
+    country,
+    tier,
+    gender,
     season,
     wk,
     date,
@@ -70,57 +81,117 @@ misc_w_refs <-
   )
 misc_w_refs
 
-misc_w_refs_agg_by_game_team <-
+agg_by_game_team <-
   misc_w_refs %>% 
-  group_by(season, referee, date, team_home, team_away, team) %>% 
+  group_by(season, country, tier, gender, referee, date, team_home, team_away, team) %>% 
   summarize(
     n_player = n_distinct(player),
     across(
-      c(mp:pk_conceded),
-      sum
+      c(mp:last_col()),
+      sum,
+      na.rm = TRUE
     )
-  ) %>% 
-  ungroup()
-misc_w_refs_agg_by_game_team
-
-misc_w_refs_agg_by_game <-
-  misc_w_refs_agg_by_game_team %>% 
-  group_by(season, referee, date, team_home, team_away) %>% 
-  summarize(
-    across(
-      c(n_player:pk_conceded),
-      sum
-    )
-  ) %>% 
-  ungroup()
-misc_w_refs_agg_by_game
-
-misc_w_refs_agg <-
-  misc_w_refs_agg_by_game %>% 
-  group_by(season, referee) %>% 
-  summarize(
-    n_game = sum(mp != 0),
-    across(c(n_player:pk_conceded), sum)
   ) %>% 
   ungroup() %>% 
+  .add_ratio_foul_tackle_col()
+agg_by_game_team
+
+agg_by_game <-
+  agg_by_game_team %>% 
+  group_by(season, country, tier, gender, referee, date, team_home, team_away) %>% 
+  summarize(
+    across(
+      c(n_player:last_col()),
+      sum
+    )
+  ) %>% 
+  ungroup()
+agg_by_game
+
+.sum <- partial(sum, na.rm = TRUE, ... =)
+agg <-
+  agg_by_game %>% 
+  group_by(season, country, tier, gender, referee) %>% 
+  summarize(
+    n_game = sum(mp != 0),
+    n_player = .sum(n_player),
+    mp = .sum(mp),
+    card_y = .sum(card_y),
+    card_r = .sum(card_r),
+    card_y2 = .sum(card_y2),
+    n_foul_committed = .sum(n_foul_committed),
+    n_foul_drawn = .sum(n_foul_drawn),
+    n_offside = .sum(n_offside),
+    n_tackle_won = .sum(n_tackle_won),
+    pk_won = .sum(pk_won),
+    pk_conceded = .sum(pk_conceded),
+    # across(c(n_player:last_col()), sum, na.rm = TRUE)
+  ) %>% 
+  ungroup() %>% 
+  .add_ratio_foul_tackle_col() %>% 
   relocate(season, referee, n_game, n_player) %>% 
   mutate(
     across(c(card_y:pk_conceded), ~11 * 90 * .x / mp)
   ) %>% 
   arrange(desc(n_game))
-misc_w_refs_agg
-misc_w_refs_agg %>% 
-  arrange(desc(card_r))
+agg
 
-misc_w_refs_agg %>% 
+agg %>% arrange(desc(card_r))
+agg %>% count(country)
+agg %>% count(tier)
+
+agg %>% skimr::skim()
+agg_filt <-
+  agg %>% 
+  filter(n_game >= 5L) %>% 
+  select(-c(card_y2, n_foul_drawn, n_tackle_won, pk_won)) %>% 
+  rename(n_pk = pk_conceded)
+app_filt
+
+agg_filt_long <-
+  agg_filt %>% 
+  pivot_longer(card_y:last_col()) # %>% 
+  # filter(value > 0)
+app_filt_long
+
+agg_filt_long %>% 
+  filter(value > 0) %>% 
+  ggplot() +
+  aes(x = value, group = country, color = country) +
+  geom_density() +
+  facet_wrap(~name, scales = 'free')
+
+agg_filt_long %>% 
+  ggplot() +
+  aes(x = value, y = country, group = country, color = country) +
+  ggbeeswarm::geom_quasirandom(groupOnX = FALSE) +
+  facet_wrap(~name, scales = 'free') +
+  theme(legend.position = 'top')
+
+agg_filt_long %>% 
+  ggplot() +
+  aes(x = value, y = tidytext::reorder_within(country, value, name), group = country, color = country) +
+  ggbeeswarm::geom_quasirandom(groupOnX = FALSE) +
+  tidytext::scale_y_reordered() +
+  facet_wrap(~name, scales = 'free') +
+  theme(legend.position = 'top')
+
+agg %>% 
+  pivot_longer(card_y:last_col()) %>% 
+  ggplot() +
+  aes(x = value, group = country, color = country) +
+  geom_density() +
+  facet_wrap(~name, scales = 'free')
+
+agg %>% 
   filter(n_game >= 10) %>% 
   skimr::skim()
 
-misc_w_refs_agg %>% 
+agg %>% 
   filter(n_game >= 10) %>% 
   filter(n_foul_drawn > n_foul_committed)
 
-misc_w_refs_agg %>% 
+agg %>% 
   filter(n_game >= 10) %>% 
   mutate(foul_ratio = n_foul_committed / n_tackle_won) %>% 
   arrange(desc(foul_ratio))
