72c887efe3da3229f9becf967852f10cc62e5acd
Tony ElHabr <anthonyelhabr@gmail.com>
2021-06-13 07:40:00 -0500

    saturday work

--- 33-2020_euros_ratings/01-main.R
+++ 33-2020_euros_ratings/01-main.R
@@ -31,45 +31,54 @@ theme_update(
 )
 update_geom_defaults('text', list(family = 'Karla', size = 4))
 
-
 f_read <- function(x) {
   file.path(dir_proj, sprintf('%s.csv', x)) %>% 
     read_csv()
 }
+
+# made this
+league_mapping <- f_read('league_mapping')
+
 leagues_init <-
   f_read('leagues') %>% 
-  rename(league_name = name) # %>% 
-  # mutate(
-  #   across(
-  #     league_name,
-  #     # Specifically for MLS playoffs
-  #     ~case_when(
-  #       country == 'USA' & league_name %>% str_detect('Major League Soccer') ~ 'Major League Soccer',
-  #       country == 'Spain' & league_name == 'Primera Division' ~ 'LaLiga',
-  #       TRUE ~ .x
-  #     )
-  #   )
-  # )
+  rename(league_name = name) %>% 
+  mutate(
+    # Some manual adjustments prior to using some "auto" name correction (by descending season)
+    across(
+      league_name,
+      ~case_when(
+        country == 'USA' & league_name %>% str_detect('Major League Soccer') ~ 'Major League Soccer',
+        country == 'China' & league_name %>% str_detect('Super League') ~ 'Super League',
+        country == 'Spain' & league_name == 'Primera Division' ~ 'LaLiga',
+        TRUE ~ .x
+      )
+    )
+  )
 leagues_init
-leagues_init %>% count(league_id)
+leagues_init %>% count(league_id, sort = TRUE)
 league_names <-
   leagues_init %>%
-  arrange(league_id, desc(season)) %>% 
+  mutate(
+    across(league_name, ~str_remove_all(.x, '\\s+[Gg]rp.*') %>% str_replace_all('eague [A-Z]$', 'eague'))
+  ) %>% 
+  mutate(n_char = nchar(league_name)) %>% 
+  arrange(league_id, n_char, desc(season)) %>% 
   group_by(league_id) %>% 
   filter(row_number() == 1L) %>% 
   ungroup() %>% 
   select(-season)
-league_names %>% filter(country == 'Spain')
+league_names
 
+leagues_init %>% select(-league_name) %>% distinct(league_id) %>% anti_join(league_names)
 leagues <- leagues_init %>% select(-league_name) %>% inner_join(league_names)
 leagues
-leagues %>% filter(country != 'International')
 
-leagues %>% filter(country == 'USA') %>% count(league_id)
 teams <- f_read('teams')
-players <- f_read('players') %>% mutate(across(dob, lubridate::date))
+players <- 
+  f_read('players') %>% 
+  mutate(across(dob, lubridate::date)) %>% 
+  rename(player_id = id, player_name = name)
 
-# TODO: Empirical bayes?
 player_ratings <-
   f_read('player_ratings') %>% 
   mutate(
@@ -77,11 +86,14 @@ player_ratings <-
   )
 player_ratings
 
+# These have multiple "sub-leagues" (e.g. playoffs, group stages).
+# This is corrected later with a summarize.
+leagues %>% count(country, league_name, season, sort = TRUE)
+
 leagues_n <- 
   leagues %>% 
-  count(country, league_name, sort = TRUE) %>% 
-  filter(league_name %>% str_detect('Europa|Champions|Playoff', negate = TRUE)) %>% 
-  filter(league_name != 'Major League Soccer') %>% 
+  filter(!(country %in% c('International', 'Europe'))) %>% 
+  count(country, league_id, league_name, sort = TRUE) %>% 
   filter(n > 2L)
 leagues_n
 
@@ -92,6 +104,8 @@ age_grps <-
   )
 age_grps
 
+# player_ratings %>% filter(player_id == 11119) %>% arrange(desc(value))
+
 position_mapping <-
   tibble(
     position_old = c('AM', 'FW', 'M', 'DM', 'D'),
@@ -99,30 +113,29 @@ position_mapping <-
   )
 position_mapping
 
+player_ratings %>% filter(league_id == 85L) %>% count(season, league_id, team_id, player_id)
+
 df_init <-
   list(
-    leagues %>% semi_join(leagues_n %>% select(-n)),
-    teams,
+    leagues_n %>% select(-n),
+    teams %>% distinct(),
     player_ratings,
-    players %>% 
-      drop_na(position, dob) %>% 
-      select(player_id = id, player_name = name, position, dob) %>% 
-      mutate(across(position, ~str_remove_all(.x, '\\(.*\\)') %>% str_remove_all('\\,.*$'))) %>% 
-      filter(position != 'GK') %>% 
-      rename(position_old = position) %>% 
-      inner_join(position_mapping) %>% 
+    players %>%
+      drop_na(position, dob) %>%
+      select(player_id, player_name, position, dob) %>%
+      mutate(across(position, ~str_remove_all(.x, '\\(.*\\)') %>% str_remove_all('\\,.*$'))) %>%
+      filter(position != 'GK') %>%
+      rename(position_old = position) %>%
+      inner_join(position_mapping) %>%
       select(-position_old)
   ) %>% 
-  reduce(inner_join) %>%  
-  group_by_at(vars(-c(games_played:value))) %>% 
-  summarize(across(where(is.numeric), sum, na.rm = TRUE)) %>% 
+  reduce(inner_join) %>%
   ungroup() %>% 
   mutate(
     value_pm = value / minutes,
     v = 90 * value_pm
   ) %>% 
   filter(minutes > (5 * 90)) %>% 
-  # filter(minutes != 0) %>% 
   mutate(age = lubridate::time_length(lubridate::ymd(sprintf('%s-08-01', season)) - dob, 'year') %>% floor() %>% as.integer()) %>% 
   filter(age >= 18 & age <= 35) %>% 
   distinct()
@@ -169,36 +182,35 @@ df <-
   df_init %>% 
   mutate(v = 90 * (value + lst$alpha) / (minutes + lst$alpha + lst$beta)) 
 
-v_min <- 
-  df %>% 
-  drop_na(v) %>% 
-  filter(!is.infinite(v)) %>% 
-  summarize(`_` = min(v)) %>% 
-  pull(`_`)
-v_min
-
-df <- df %>% mutate(across(v, ~log(.x + abs(!!v_min) + 0.001)))
-df
-
-agg <-
-  df %>% 
-  group_by(league_id, league_name, country, season, position, age_grp) %>% 
-  summarize(
-    n = n(),
-    across(
-      v,
-      list(mean = mean, sd = sd), na.rm = TRUE, .names = '{.fn}'
-    )
-  ) %>% 
-  ungroup() %>% 
-  filter(n > 1L)
-agg
-
-df <-
-  df %>% 
-  inner_join(agg) %>%
-  mutate(z = (v - mean) / sd)
-df
+# # Commenting this shit out to try direct vaep comparison
+# v_min <- 
+#   df %>% 
+#   drop_na(v) %>% 
+#   filter(!is.infinite(v)) %>% 
+#   summarize(`_` = min(v)) %>% 
+#   pull(`_`)
+# v_min
+# df <- df %>% mutate(across(v, ~log(.x + abs(!!v_min) + 0.001)))
+# agg <-
+#   df %>% 
+#   group_by(league_id, league_name, country, season, position, age_grp) %>% 
+#   summarize(
+#     n = n(),
+#     across(
+#       v,
+#       list(mean = mean, sd = sd), na.rm = TRUE, .names = '{.fn}'
+#     )
+#   ) %>% 
+#   ungroup() %>% 
+#   filter(n > 1L)
+# agg
+# 
+# df <-
+#   df %>% 
+#   inner_join(agg) %>%
+#   mutate(z = (v - mean) / sd)
+# df
+df <- df %>% rename(z = v)
 
 df %>% count(age_grp, sort = TRUE)
 df %>% count(position, sort = TRUE)
@@ -211,10 +223,23 @@ df %>%
 df
 
 df %>% 
+  mutate(across(position, factor)) %>% 
+  ggplot() +
+  aes(x = z, color = position) +
+  geom_density() +
+  coord_cartesian(xlim = c(-3, 3))
+df
+
+df %>% 
   ggplot() +
   aes(x = z) +
   geom_histogram(binwidth = 0.1) +
   coord_cartesian(xlim = c(-3, 3))
+
+df %>% 
+  arrange(desc(z)) %>% 
+  relocate(player_name) %>% 
+  filter(country == 'England', league_name == 'Premier League', season == 2021)
 
 ids <-
   df %>% 
@@ -235,7 +260,10 @@ ids_gt1_meta <-
     df %>% 
       select(player_id, league_id, country, league_name, season, z) %>% 
       unite('league', country, league_name, sep = '_') %>% 
-      mutate(across(league, ~str_replace_all(.x, '\\s|[.]', '_') %>% str_replace_all('[_]+', '_')))
+      mutate(across(league, ~str_replace_all(.x, '\\s|[.]', '_') %>% str_replace_all('[_]+', '_'))) %>% 
+      left_join(league_mapping) %>% 
+      select(-league) %>% 
+      rename(league = league_lab)
   )
 ids_gt1_meta %>% count(league, sort = TRUE)
 
@@ -271,7 +299,7 @@ dummies <-
   pivot_wider(names_from = value, values_from = name, values_fill = 0L) %>% 
   select(-idx) %>% 
   # Make this the NA coefficient
-  relocate(Netherlands_Eredivisie, .after = last_col())
+  relocate(`Eredivisie (Netherlands)`, .after = last_col())
 dummies
 
 fit_dummy <- dummies %>% lm(formula(z_diff ~ . - season), data = .)
@@ -281,6 +309,7 @@ extract_coefs <- function(fit) {
   fit %>% 
     broom::tidy() %>% 
     select(league = term, estimate) %>% 
+    mutate(across(league, ~str_remove_all(.x, '`'))) %>% 
     arrange(desc(estimate))
 }
 
@@ -308,7 +337,7 @@ p_coefs
 
 ggsave(
   plot = p_coefs,
-  filename = file.path(dir_proj, 'coefs.png'),
+  filename = file.path(dir_proj, 'coefs_vp90.png'),
   width = 8,
   height = 8,
   type = 'cairo'
@@ -330,29 +359,46 @@ coefs_by_season <-
   # mutate(across(season, factor))
   mutate(across(season, ~sprintf('%04d-07-01', .x) %>% lubridate::ymd()))
 coefs_by_season
+coefs_by_season %>% filter(season == max(season))
 
-leagues_filt <- c('England_Premier_League', 'Spain_LaLiga', 'Italy_Serie_A', 'Germany_1_Bundesliga', 'France_Ligue_1')
+leagues_filt <- c('Premier League (England)', 'Serie A (Italy)', 'Bundesliga 1 (Germany)', 'Ligue 1 (France)', 'La Liga (Spain)')
 coefs_by_season_filt <-
   coefs_by_season %>% 
   filter(league %in% leagues_filt)
+coefs_by_season_filt %>% filter(season == max(season)) # make sure colors follow this order
 
 coefs_by_season_nofilt <-
   coefs_by_season %>% 
   filter(!(league %in% leagues_filt))
 
-seq_date_first <- seq.Date(lubridate::ymd('2010-01-01'), lubridate::ymd('2021-01-01'), by = 'year')
-seq_date_mid <- seq.Date(lubridate::ymd('2010-07-01'), lubridate::ymd('2020-07-01'), by = 'year')
+seq_date_first <- seq.Date(lubridate::ymd('2010-01-01'), lubridate::ymd('2022-01-01'), by = 'year')
+seq_date_mid <- seq.Date(lubridate::ymd('2010-07-01'), lubridate::ymd('2021-07-01'), by = 'year')
 seq_date_mid
 seq_date <- sort(c(seq_date_first, seq_date_mid))
 seq_date
-labs_date <- c(rep(2010:2020, each = 2), 2021)
+labs_date <- c(rep(2010:2021, each = 2), 2022)
 labs_date
 idx <- (seq_along(labs_date) %% 2) == 1L
 idx
+idx[length(idx) - 1] <- TRUE
 labs_date[idx] <- ''
 labs_date
+labs_date
+
+f_text <- partial(
+  ggrepel::geom_text_repel,
+  # label.color = NA,
+  # fill = NA,
+  family = 'Karla',
+  fontface = 'bold',
+  # hjust = 0,
+  ... = 
+)
 
 pal <- c('#003f5c', '#58508d', '#bc5090', '#ff6361', '#ffa600')
+pal <- c('#003f5c', '#58508d', '#bc5090', '#ff6361', '#ffa600') %>% setNames(leagues_filt)
+pal %>% scales::show_col()
+require(lubridate)
 p_coefs_by_season <-
   coefs_by_season_nofilt %>% 
   ggplot() +
@@ -369,24 +415,33 @@ p_coefs_by_season <-
     size = 1.25,
     aes(color = league)
   ) +
+  f_text(
+    data = 
+      coefs_by_season_filt %>% 
+      filter(season == max(season)) %>% 
+      mutate(across(season, ~.x %m+% months(1))),
+    hjust = 0,
+    aes(label = league, color = league)
+  ) +
   scale_color_manual(values = pal) +
-  scale_x_date(labels = labs_date, breaks = seq_date, limits = range(seq_date)) +
-  guides(color = guide_legend(title = '', override.aes = list(size = 3))) +
+  scale_x_date(labels = labs_date, breaks = seq_date, limits = range(lubridate::ymd('2013-01-01'), lubridate::ymd('2022-01-01'))) +
+  # guides(color = guide_legend(title = '', override.aes = list(size = 3))) +
+  guides(color = FALSE) +
   theme(
+    panel.grid.major.y = element_blank(),
     axis.ticks.x = element_line(color = c('grey80', rep(c(NA, 'grey80'), t = length(seq_date)))),
-    panel.grid.major.x = element_line(color = c('grey80', rep(c(NA, 'grey80'), t = length(seq_date)))),
-    legend.position = 'top'
+    panel.grid.major.x = element_line(color = c('grey80', rep(c(NA, 'grey80'), t = length(seq_date))))
   ) +
   labs(
     title = 'League Strength Coefficients',
-    y = 'Power Coefficient',
+    y = 'Coefficient',
     x = NULL
   )
 p_coefs_by_season
 
 ggsave(
   plot = p_coefs_by_season,
-  filename = file.path(dir_proj, 'coefs_by_year.png'),
+  filename = file.path(dir_proj, 'coefs_by_year_vp90.png'),
   width = 12,
   height = 8,
   type = 'cairo'
