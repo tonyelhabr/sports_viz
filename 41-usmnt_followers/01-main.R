cd6106abbb38101a1136f6b44788d279f9dc51e9
Tony ElHabr <anthonyelhabr@gmail.com>
2021-09-23 07:10:41 -0500

    finish 41

--- 41-usmnt_followers/01-main.R
+++ 41-usmnt_followers/01-main.R
@@ -1,25 +1,47 @@
 
 library(tidyverse)
 library(rtweet)
-# library(xengagement)
-# token <- get_twitter_token()
+library(xengagement)
+token <- get_twitter_token()
 
 dir_proj <- '41-usmnt_followers'
+source(file.path(dir_proj, 'helpers.R'))
 
-# watke <- 'watke_'
+download_pic <- function(user, pic, overwrite = FALSE) {
+  path <- file.path(dir_proj, 'img', sprintf('%s.jpg', user))
+  if(file.exists(path) & !overwrite) {
+    return((path))
+  }
+  download.file(pic, destfile = path, quiet = TRUE, mode = 'wb')
+}
+
+watke <- 'watke_'
+path_watke_followers <- file.path(dir_proj, 'watke_followers.rds')
 # watke_folowers <- get_followers(user, n = 18000)
 # write_rds(folowers, path_watke_followers)
-watke_folowers <- read_rds(path_followers)
-
-usmnt <- 'USMNT'
-usmnt_followers <- get_followers(usmnt, n = 2200000, retryonratelimit = TRUE)
-path_usmnt_followers <- file.path(dir_proj, 'usmnt_followers.rds')
-write_rds(usmnt_followers, path_usmnt_followers)
+watke_followers <- read_rds(path_watke_followers)
 
 lasso <- 'TedLasso'
-lasso_followers <- get_followers(lasso, n = 525000, retryonratelimit = TRUE)
 path_lasso_followers <- file.path(dir_proj, 'lasso_followers.rds')
-write_rds(lasso_followers, path_lasso_followers)
+# lasso_followers <- get_followers(lasso, n = 525000, retryonratelimit = TRUE)
+# last lasso next_cursor was "1690687164766806439"
+# lasso_followers <- get_all_followers(lasso, followers = 525000, page = '-1')
+# write_rds(lasso_followers, path_lasso_followers)
+lasso_followers <- read_rds(path_lasso_followers)
+
+usmnt <- 'USMNT'
+path_usmnt_followers <- file.path(dir_proj, 'usmnt_followers.rds')
+# usmnt_followers <- get_followers(usmnt, n = Inf, retryonratelimit = TRUE)
+usmnt_followers <- get_all_followers(usmnt, followers = 2200000, page = '-1')
+usmnt_followers <- usmnt_followers[1:29] %>% map_dfr(bind_rows)
+# next cursor was "1701898738548249542"
+write_rds(usmnt_followers, path_usmnt_followers)
+usmnt_followers <- read_rds(path_usmnt_followers)
+
+n_followers_watke <- lookup_users(watke)$followers_count
+n_followers_lasso <- lookup_users(lasso)$followers_count
+n_followers_usmnt <- lookup_users(usmnt)$followers_count
+total_followers <- n_followers_watke + n_followers_lasso + n_followers_usmnt
 
 df <-
   bind_rows(
@@ -40,7 +62,8 @@ df <-
 #   ) %>%
 #   reduce(full_join)
 
-df %>% 
+grps <-
+  df %>% 
   mutate(
     grp = case_when(
       is.na(watke) & is.na(lasso) ~ 'usmnt only',
@@ -51,5 +74,114 @@ df %>%
       is.na(usmnt) ~ 'watke + lasso',
       TRUE ~ 'all'
     )
+  )
+grps
+
+cnts_init <-
+  grps %>% 
+  count(grp, sort = TRUE) %>% 
+  mutate(
+    n_actual = case_when(
+      str_detect(grp, 'only') ~ 1L,
+      grp == 'all' ~ 3L,
+      TRUE ~ 2L
+    ),
+    n_adj = n * n_actual,
+    frac = n_adj / sum(n_adj)
+  )
+cnts_init
+
+# for percentages
+cnts_watke <-
+  cnts_init %>% 
+  filter(grp == 'all' | str_detect(grp, 'watke')) %>% 
+  mutate(
+    frac = n / sum(n),
+    n_boost = frac * n_followers_watke,
+    n_boost_adj = n_boost / n_actual
+  )
+cnts_watke
+
+cnts_lasso <-
+  cnts_init %>% 
+  filter(grp == 'all' | str_detect(grp, 'lasso')) %>% 
+  mutate(
+    frac = n / sum(n),
+    n_boost = frac * n_followers_lasso,
+    n_boost_adj = n_boost / n_actual
+  )
+cnts_lasso
+
+cnts_usmnt <-
+  cnts_init %>% 
+  filter(grp == 'all' | str_detect(grp, 'usmnt')) %>% 
+  mutate(
+    frac = n / sum(n),
+    n_boost = frac * n_followers_usmnt,
+    n_boost_adj = n_boost / n_actual
+  )
+cnts_usmnt
+
+# need to boost lass only from 465k to 470k for some reason
+cnts_adj <-
+  bind_rows(
+    cnts_watke,
+    cnts_lasso,
+    cnts_usmnt
   ) %>% 
-  count(grp, sort = TRUE)
+  group_by(grp, n, n_actual) %>% 
+  summarize(
+    across(n_boost, sum),
+    across(n_boost_adj, sum)
+  ) %>% 
+  ungroup() %>% 
+  mutate(n_boost_adj = ifelse(grp == 'lasso only', 490000, n_boost_adj))
+cnts_adj
+cnts_adj_watke <-
+  cnts_adj %>% 
+  filter(grp == 'all' | str_detect(grp, 'watke')) %>% 
+  mutate(
+    frac = n_boost_adj / sum(n_boost_adj)
+  )
+cnts_adj_watke
+
+cnts_adj_lasso <-
+  cnts_adj %>% 
+  filter(grp == 'all' | str_detect(grp, 'lasso')) %>% 
+  mutate(
+    frac = n_boost_adj / sum(n_boost_adj)
+  )
+cnts_adj_lasso
+
+cnts_adj_usmnt <-
+  cnts_adj %>% 
+  filter(grp == 'all' | str_detect(grp, 'usmnt')) %>% 
+  mutate(
+    frac = n_boost_adj / sum(n_boost_adj)
+  )
+cnts_adj_usmnt
+
+
+total_followers - sum(cnts_adj$n_boost)
+
+sum(cnts_init$n_adj)
+frac_boost <- total_followers / sum(cnts_init$n_adj)
+frac_boost
+cnts <- 
+  cnts_init %>% 
+  mutate(n_boost = frac * total_followers, n_boost_adj = n_boost / n_actual)
+cnts
+sum(cnts$n_boost)
+total_followers
+grps %>% filter(grp == 'all')
+df %>% filter(!is.na(watke))
+grps %>% filter(grp == 'watke + usmnt') %>% filter(!is.na(lasso))
+
+# extra ----
+pundits <- lookup_users(c('AlexiLalas', 'TaylorTwellman', 'GrantWahl'))
+pundits
+grps %>% 
+  filter(user_id %in% pundits$user_id)
+
+players <- lookup_users(c('ChristainPulisic', 'GioReyna', 'Weston McKinnie'))
+players
---
+++ 41-usmnt_followers/helpers.R
@@ -0,0 +1,100 @@
+
+# https://stackoverflow.com/questions/45152331/how-to-get-all-the-twitter-followers-ids-75000-of-a-user-using-pagination-in
+library(rtweet)
+# Global variables:
+ids <- 75000 # Max number of ids per token every 15 minutes
+res <- list() # Vector where user_ids from followers will be appended
+
+# Function to get all the followers from a user with pagination
+get_all_followers <- function(userId, followers, page) {
+  
+  if (ids == 0) {
+    
+    # API Twitter Limit reached - Wait
+    message("Waiting 15 mins...")
+    total <- 15*60 # Total time = 15 min ~ 900 sec
+    pb <- txtProgressBar(min = 0, max = total, style = 3) # create progress bar
+    
+    for (i in 1:total) {
+      Sys.sleep(time = 1) # 1 second interval
+      setTxtProgressBar(pb, i) # update progress bar
+    }
+    close(pb)
+    
+    # Check rate limit followers/ids query
+    if (!rate_limit(token = NULL)[38,]$reset > 14.9) {
+      message("Waiting 15 seconds more...")
+      Sys.sleep(time = 15) # wait 15 seconds more...
+    }
+    
+    message("Go!")
+    ids <<- 75000
+  }
+  
+  if (followers <= ids) {
+    
+    message(paste("Followers < ids | Number of Followers: ",
+                  followers, " | Number of resting ids: ",  ids, sep = ""))
+    ftemp <- get_followers(user = userId, n = followers, page = page)
+    
+    if (page == '-1') {
+      res <<- append(res, list(ftemp)) # append followers ids
+    }
+    
+    if (page != '-1') {
+      # df <- data.frame('user_id' = ftemp)
+      
+      res <<- append(res, list(df)) # append followers ids
+    }
+    
+    ids <<- ids - followers
+    message("Finished!")
+    rtemp <- res
+    res <<- list()
+    return(rtemp)
+    
+  } else if (followers > ids) {
+    
+    message(paste("Followers > ids | Number of Followers: ",
+                  followers, " | Number of resting ids: ",  ids, sep = ""))
+    ftemp <- get_followers(user = userId, n = ids, page = page)
+    
+    if (page == '-1') {
+      res <<- append(res, list(ftemp)) # append followers ids
+    }
+    
+    if (page != '-1') {
+      # browser()
+      # df <- data.frame('user_id' = ftemp)
+      res <<- append(res, list(ftemp)) # append followers ids
+    }
+    
+    n <- ids # n = count of followers ids already acquired
+    
+    pageTemp <- next_cursor(ftemp) # Pagination
+    print(pageTemp)
+    # browser()
+    # API Twitter Limit reached - Wait
+    message("Waiting 15 mins...")
+    total <- 15*60 # Total time = 15 min ~ 900 sec
+    pb <- txtProgressBar(min = 0, max = total, style = 3) # create progress bar
+    
+    for (i in 1:total) {
+      Sys.sleep(time = 1) # 1 second interval
+      setTxtProgressBar(pb, i) # update progress bar
+    }
+    close(pb)
+    
+    # Check rate limit followers/ids query
+    if (!rate_limit(token = NULL)[38,]$reset > 14.9) {
+      message("Waiting 15 seconds more...")
+      Sys.sleep(time = 15) # wait 15 seconds more...
+    }
+    
+    message("Go!")
+    ids <<- 75000
+    
+    # Recursive function call
+    get_all_followers(userId = userId, followers = followers - n, page = pageTemp)
+  }
+}
