cc47fec7c850204a1d2e18459bdf961d6984f0c1
Tony ElHabr <anthonyelhabr@gmail.com>
2021-10-20 09:08:17 -0500

    full caley

--- 43-xg_signal/02-process_fbref.R
+++ 43-xg_signal/02-process_fbref.R
@@ -1,7 +1,7 @@
 
 library(tidyverse)
-
 library(sqldf)
+
 # library(tonythemes)
 # tonythemes::theme_set_tony()
 dir_proj <- '43-xg_signal'
@@ -21,8 +21,10 @@ results
 
 results_slim <-
   results %>% 
-  filter(country == 'ENG') %>% 
+  # filter(country == 'ENG') %>% 
+  filter(country != 'USA') %>% 
   drop_na(home_x_g) %>% 
+  drop_na(wk) %>% 
   select(
     league = country,
     season = season_end_year,
@@ -65,27 +67,21 @@ results_redux <-
     ),
     pts = w * 3L + d * 1L + l * 0L,
     gd = g - g_opp,
-    xgd = xg - xg_opp
+    gr = g / g_opp,
+    xgd = xg - xg_opp,
+    xgr = xg / xg_opp
   ) %>% 
   group_by(season, league, tm) %>% 
   mutate(
     mw = row_number(date),
-    across(c(w, d, l, pts, g, gd, xg, xgd), list(cumu = cumsum))
+    across(c(w, d, l, pts, matches('^x?g[dr]?')), list(cumu = cumsum))
   ) %>% 
   ungroup() %>% 
   group_by(league, tm) %>% 
-  mutate(mw_total = row_number(season + mw/100)) %>% 
+  mutate(mw_total = row_number(season + mw / 100)) %>% 
   ungroup() %>% 
-  mutate(
-    gdpm_cumu = gd_cumu / mw,
-    xgpm_cumu = xg_cumu / mw,
-    xgdpm_cumu = xgd_cumu / mw
-  ) %>% 
   group_by(season, league, mw) %>% 
   mutate(
-    rnk_gd = dense_rank(-gdpm_cumu),
-    rnk_xg = dense_rank(-xgpm_cumu),
-    rnk_xgd = dense_rank(-xgpm_cumu),
     rnk1 = dense_rank(-pts_cumu),
   ) %>% 
   ungroup() %>% 
@@ -104,75 +100,18 @@ results_redux <-
   ) %>% 
   select(-matches('^rnk[123]|^[wdl]$|^[wdl]_cumu$'))
 results_redux
-results_redux %>% filter(league == 'ENG') %>% count(league, season, mw) %>% count(n, name = 'nn')
 
-eoy_standings <- 
-  results_redux %>% 
-  group_by(league, season) %>% 
-  slice_max(mw) %>%
-  ungroup() %>%
-  select(
-    league,
-    season,
-    tm,
-    gdpm_eoy = gdpm_cumu,
-    rnk_gd_eoy = rnk_gd,
-    xgpm_eoy = xgpm_cumu,
-    rnk_xg_eoy = rnk_xg,
-    xgdpm_eoy = xgdpm_cumu,
-    rnk_xgd_eoy = rnk_xgd,
-    pts_eoy = pts_cumu,
-    rnk_eoy = rnk_cumu
-  ) %>%
-  arrange(league, season, rnk_eoy)
-eoy_standings
-
-cors <-
-  results_redux %>% 
-  left_join(eoy_standings) %>% 
-  # head(1000) %>% 
-  select(all_of(names(eoy_standings)), -tm, mw, gdpm_cumu, xgpm_cumu, xgdpm_cumu, rnk_cumu) %>%  
-  group_nest(league, mw) %>% 
-  mutate(
-    pearson = map(data, corrr::correlate, method = 'pearson', quiet = TRUE),
-    spearman = map(data, corrr::correlate, method = 'spearman', quiet = TRUE)
-    # stretch = map(cors, corrr::stretch)
-  ) %>% 
-  select(-c(data))
-cors
-
-cors_long <-
-  cors %>% 
-  pivot_longer(
-    -c(league, mw),
-    names_to = 'method',
-    values_to = 'cors'
-  ) %>% 
-  unnest(cors) %>% 
-  rename(col1 = term) %>% 
-  pivot_longer(
-    -c(league, mw, method, col1),
-    names_to = 'col2',
-    values_to = 'cor'
-  ) %>% 
-  filter(col1 != col2)
-cors_long
-
-cors_long %>% 
-  # filter(col1 == 'rnk_eoy') %>% 
-  filter(col1 == 'xgdpm_eoy') %>% 
-  filter(col2 %>% str_detect('cumu')) %>% 
-  ggplot() +
-  aes(x = mw, y = cor^2, color = method) +
-  # geom_line(aes(color = factor(league))) +
-  geom_hline(aes(yintercept = 0.5)) +
-  geom_point() +
-  geom_line() +
-  facet_wrap(~col2, scales = 'fixed')
+# results_redux %>% 
+#   filter(league == 'GER') %>% 
+#   filter(!is.na(wk)) %>% 
+#   # filter(league == 'GER') %>% 
+#   # filter(season == 2021, mw == 2)
+#   count(league, season, mw) %>%
+#   count(n, name = 'nn')
 
 df <-
   sqldf::sqldf(
-    'select a.league, a.season, a.tm, a.mw, b.mw as mw_post, a.mw_total, b.mw_total as b.mw_total_post, a.g, b.g as g_post, a.gd, b.gd as gd_post, a.xg, b.xg as xg_post, a.xgd, b.xgd as xgd_post
+    'select a.league, a.season, a.tm, a.mw, b.mw as mw_post, a.mw_total, b.mw_total as mw_total_post, a.g, b.g as g_post, a.gd, b.gd as gd_post, a.xg, b.xg as xg_post, a.xgd, b.xgd as xgd_post
      from results_redux a
      inner join results_redux b
      on a.league = b.league and a.season = b.season and a.tm = b.tm and a.mw < (b.mw + 20)
@@ -183,49 +122,26 @@ df %>% count(mw, sort = TRUE)
 df %>% 
   filter(tm == 'Manchester City') %>% 
   # count(mw, sort = TRUE)
-  filter(mw == 1, season == 2021)
+  filter(mw == 38, season == 2021)
 
-agg_pre <- 
+w <- 20
+results_slide <-
   results_redux %>% 
-  arrange(league, season, tm, mw) %>% 
-  select(league, season, tm, mw, g, gd, xg, xgd) %>% 
-  rename_with(~sprintf('%s_pre', .x), c(g, gd, xg, xgd)) %>% 
-  group_by(league, season, tm) %>% 
+  group_by(league, tm) %>% 
   mutate(
-    across(matches('_pre$'), cumsum)
+    across(c(g, gd, xg, xgd, pts), list(w = ~slider::slide_dbl(.x, sum, .after = !!w, .complete = TRUE)))
   ) %>% 
   ungroup() %>% 
+  # filter(is.na(g_w)) %>% 
+  drop_na(g_w) %>% 
   mutate(
-    across(matches('_pre$'), ~.x / mw)
+    across(matches('_cumu$'), ~.x / mw),
+    across(matches('_w'), ~.x / !!w)
   )
-agg_pre
-
-agg_post <-
-  df %>% 
-  select(-mw_post) %>% 
-  group_by(league, season, tm, mw) %>% 
-  summarize(
-    n = n(),
-    across(matches('_post$'), sum)
-  ) %>% 
-  ungroup() %>% 
-  mutate(
-    # across(matches('^[x]?g[d]?$'), ~.x / mw),
-    across(matches('_post$'), ~.x / n)
-  ) %>% 
-  arrange(league, season, tm, mw)
-agg_post
-
-agg <-
-  full_join(
-    agg_pre %>% filter(mw != 38L),
-    agg_post %>% select(-c(n))
-  )
-agg
 
 cors <-
-  agg %>% 
-  select(-tm) %>%  
+  results_slide %>% 
+  select(league, season, mw, matches('(^x?gd?|pts)_(cumu|w)')) %>% 
   group_nest(league, season, mw) %>% 
   mutate(
     cors = map(data, corrr::correlate, method = 'pearson', quiet = TRUE)
@@ -238,7 +154,7 @@ cors_long <-
   unnest(cors) %>% 
   rename(col1 = term) %>% 
   pivot_longer(
-    -c(league, season, mw, col1),
+    -c(league, mw, col1),
     names_to = 'col2',
     values_to = 'cor'
   ) %>% 
@@ -249,20 +165,42 @@ cors_long <-
   separate(
     col2, into = c('stat2', 'state2'), sep = '_'
   ) %>% 
-  filter(state1 == 'pre' & state2 == 'post') %>% 
+  filter(state1 == 'cumu' & state2 == 'w') %>% 
   mutate(r2 = cor^2)
 cors_long
 
 cors_long %>% 
   filter(stat1 == 'xg') %>% 
-  filter(mw <= 20) %>% 
   ggplot() +
   geom_hline(aes(yintercept = 0.5)) +
-  aes(x = mw, y = r2, color = stat2) +
+  aes(x = mw, y = r2, color = league) +
   geom_line() +
   geom_point() +
-  facet_wrap(~season)
+  facet_wrap(~stat2)
 
+
+cors_long %>% 
+  filter(stat1 == 'xg', stat2 == 'xgd') %>% 
+  ggplot() +
+  geom_hline(aes(yintercept = 0.5)) +
+  aes(x = mw, y = r2, color = league) +
+  geom_line() +
+  geom_point()
+
+
+cors_long %>% 
+  filter(stat1 == 'xg', stat2 == 'xgd') %>% 
+  group_by(mw) %>% 
+  summarize(
+    across(r2, mean)
+  ) %>% 
+  ungroup() %>% 
+  ggplot() +
+  geom_hline(aes(yintercept = 0.5)) +
+  aes(x = mw, y = r2, group = stat2, color = state2) +
+  # geom_line() +
+  # geom_point() +
+  geom_smooth(formula = y ~ log(x), method = 'lm')
 
 # stats <-
 #   fs::dir_ls(dir_data, regexp = 'misc-team*rds$') %>%
