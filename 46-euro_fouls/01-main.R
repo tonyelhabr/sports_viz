4e4411587ee07f26222448f7c2e2f417c4682fb0
Tony ElHabr <anthonyelhabr@gmail.com>
2021-12-29 13:20:28 -0600

    finish rage quit

--- 46-euro_fouls/01-main.R
+++ 46-euro_fouls/01-main.R
@@ -1,9 +1,9 @@
 
 library(tidyverse)
 library(arrow)
-library(ebbr)
-library(rPref)
-library(tonythemes)
+# library(ebbr)
+# library(rPref)
+library(ggfittext)
 
 gray_wv <- rgb(24, 24, 24, maxColorValue = 255)
 gray_grid_wv <- rgb(64, 64, 64, maxColorValue = 255)
@@ -13,9 +13,9 @@ theme_set(theme_minimal())
 theme_update(
   text = element_text(family = 'Karla', color = 'white'),
   title = element_text('Karla', size = 14, color = 'white'),
-  plot.title = element_text('Karla', face = 'bold', size = 16, color = 'white'),
+  plot.title = element_text('Karla', face = 'bold', size = 24, color = 'white'),
   plot.title.position = 'plot',
-  plot.subtitle = element_text('Karla', size = 14, color = '#f1f1f1'),
+  plot.subtitle = element_text('Karla', size = 18, color = '#f1f1f1'),
   axis.text = element_text('Karla', color = 'white', size = 14),
   axis.title = element_text('Karla', size = 14, color = 'white', face = 'bold', hjust = 0.99),
   axis.line = element_blank(),
@@ -48,39 +48,9 @@ c(
 ) %>% 
   walk(f_import)
 
-
-all_actions <- all_actions %>% 
-  mutate(
-    half = case_when(
-      team_id == home_team_id & start_x >= 1 * 105 / 2 ~ 'attacking',
-      team_id == away_team_id & start_x <= 1 * 105 / 2 ~ 'attacking',
-      TRUE ~ 'defensive'
-    ),
-    third = case_when(
-      team_id == home_team_id & start_x >= 2 * 105 / 3 ~ 'attacking',
-      team_id == home_team_id & start_x >= 1 * 105 / 3 ~ 'middle',
-      team_id == away_team_id & start_x <= 1 * 105 / 3 ~ 'attacking',
-      team_id == away_team_id & start_x <= 2 * 105 / 3 ~ 'middle',
-      TRUE ~ 'defensive'
-    )
-  )
-
-fouls <- all_actions %>% filter(type_name == 'foul')
-turnovers <- all_actions %>% filter(turnover)
-
-euro_fouls <- all_actions %>%
-  filter(
-    type_name == 'foul',
-    time_between_poss <= 5,
-    time_since_poss_start <= 3
-  )
-euro_fouls
-
 rage_quits <- all_actions %>% 
   filter(
     type_name == 'foul',
-    # time_between_poss <= 5,
-    # time_since_poss_start <= 3,
     turnover_player_id == player_id,
     time_since_last_turn <= 7
   ) %>% 
@@ -88,87 +58,96 @@ rage_quits <- all_actions %>%
 rage_quits
 
 n_rage_quits <- rage_quits %>% 
-  # filter(season_id == 2018) %>% 
   count(competition_id, season_id, player_id, player_name, sort = TRUE) %>% 
   left_join(players_filt)
 n_rage_quits
 
 top_n_rage_quits <- n_rage_quits %>% 
-  filter(season_id > 2017) %>% 
+  # filter(season_id > 2017) %>% 
   filter(n == 4) %>% 
   slice_min(
     total_mp,
-    n = 7,
+    n = 4,
     with_ties = FALSE
   ) %>% 
   bind_rows(
     n_rage_quits %>% filter(n == 5)
   ) %>% 
   mutate(
-    lab = sprintf("%s '%s", str_replace_all(player_name, '(^.*\\s)(.*$)', '\\2'), season_id)
+    lab = sprintf("%s '%s", str_replace_all(player_name, '(^.*\\s)(.*$)', '\\2'), str_sub(season_id, 3, 4))
   )
-
-# top_n_rage_quits <- n_rage_quits %>% 
-#   filter(season_id > 2017) %>% 
-#   filter(n == 4) %>% 
-#   group_by(season_id) %>% 
-#   # slice_max(
-#   #   n,
-#   #   n = 1
-#   # ) %>% 
-#   slice_min(
-#     total_mp,
-#     n = 2,
-#     with_ties = FALSE
-#   ) %>% 
-#   ungroup() %>% 
-#   bind_rows(
-#     n_rage_quits %>% filter(n == 5)
-#   ) %>% 
-#   mutate(
-#     lab = sprintf("%s '%s", str_replace_all(player_name, '(^.*\\s)(.*$)', '\\2'), season_id)
-#   )
-# top_n_rage_quits
-
+cancelo_rage_quits <- top_n_rage_quits %>% filter(player_name == 'Joao Cancelo')
+top_n_rage_quits_wo_cancelo <- top_n_rage_quits %>% anti_join(cancelo_rage_quits)
 n_rage_quits_minus_top <- n_rage_quits %>% anti_join(top_n_rage_quits) 
 pal <- c('#ef426f', '#00b2a9', '#ff8200', '#7a5195') %>% rev() # spurs fiesta + purple
 pts <- function(x) {
   as.numeric(grid::convertUnit(grid::unit(x, 'pt'), 'mm'))
 }
-n_rage_quits_minus_top %>% 
-  ggplot() +
-  aes(x = total_mp, y = n, alpha = n, color = factor(n)) +
-  geom_point(size = 1.5) +
-  geom_segment(
-    aes(xend = 0, yend = 0)
-  ) +
-  scale_color_manual(values = pal) +
-  guides(color = 'none', alpha = 'none') +
-  geom_point(
-    data = top_n_rage_quits,
-    size = 2,
-    color = 'black'
-  ) +
-  geom_segment(
-    data = top_n_rage_quits,
-    alpha = 0.5,
-    aes(xend = 0, yend = 0, color = factor(n)),
-    color = 'black'
-  ) +
-  ggrepel::geom_text_repel(
-    data = top_n_rage_quits,
-    family = 'Karla',
-    color = 'black',
-    size = pts(14),
-    fontface = 'bold',
-    # label.size = NA,
-    seed = 42, 
-    # box.padding = 0,
-    aes(label = lab, y = n)
-  ) +
-  coord_polar() +
+
+y_labs <- c('0', '1,000', '2,000 minutes played', '3,000', '4,000')
+base_plot <- function(data, point.size = 1, segment.size = 0.5, ...) {
+  data %>% 
+    ggplot() +
+    aes(x = total_mp, y = n, alpha = n, color = factor(n)) +
+    coord_polar() +
+    geom_point(size = point.size) +
+    geom_segment(
+      size = segment.size,
+      aes(xend = 0, yend = 0)
+    ) +
+    scale_color_manual(values = pal) +
+    guides(color = 'none', alpha = 'none') +
+    theme(
+      axis.text.y = element_blank()
+    ) +
+    scale_x_continuous(
+      limits = c(0, 4000), 
+      labels = y_labs
+    )
+}
+
+add_labels <- function(..., .data, .color = 'white') {
+  list(
+    ...,
+    geom_point(
+      data = .data,
+      size = 2,
+      alpha = 1,
+      color = .color
+    ),
+    geom_segment(
+      data = .data,
+      # alpha = 0.5,
+      size = 1,
+      alpha = 0.5,
+      aes(xend = 0, yend = 0),
+      color = .color
+    ),
+    ggrepel::geom_text_repel(
+      data = .data,
+      family = 'Karla',
+      color = .color,
+      size = pts(16),
+      fontface = 'bold',
+      seed = 42,
+      # vjust = 1.5,
+      # hjust = -.5,
+      alpha = 1,
+      aes(label = lab, y = n + 0.7, x = total_mp - 100)
+    )
+  )
+}
+
+cancelo_blue <- '#6caddf'
+p <- n_rage_quits_minus_top %>% 
+  base_plot() +
+  add_labels(.data = top_n_rage_quits_wo_cancelo, .color = 'white') +
+  add_labels(.data = cancelo_rage_quits, .color = cancelo_blue) +
   geom_text(
     inherit.aes = FALSE,
+    color = 'white',
+    fontface = 'bold',
+    size = pts(14),
     data = tibble(x = 100, y = 1:5) %>% 
       mutate(lab = ifelse(y == 5, '5 "rage" fouls', y)),
     aes(x = x, y = y, label = lab),
@@ -177,15 +156,126 @@ n_rage_quits_minus_top %>%
     vjust = -1,
     hjust = 0
   ) +
-  theme(
-    axis.text.y = element_blank()
-  ) +
-  scale_x_continuous(limits = c(0, 4000)) +
-  scale_y_continuous(limits = c(0, 5)) +
   labs(
     y = NULL,
-    x = 'Minutes Played'
+    x = NULL
+  ) +
+  theme(
+    plot.title = element_text(hjust = 0),
+    plot.subtitle = ggtext::element_markdown(hjust = 0),
+    plot.caption = ggtext::element_markdown(size = 14, face = 'plain')
+  ) +
+  labs(
+    title = 'Who "rage" fouls most frequently?',
+    subtitle = glue::glue('<span style="color:{cancelo_blue}"><b>Cancelo</b></span> has commited the most "rage" fouls (4)<br>in the 2021/22 Premier League season.'),
+    caption = '<i>"Rage" foul: a foul within 7 seconds of own turnover</i><br/><br/><b>Viz</b>: Tony ElHabr'
   )
+
+path_rage_quit <- file.path(dir_proj, 'rage_quit.png')
+ggsave(
+  plot = p,
+  filename = path_rage_quit,
+  w = 10,
+  h = 10
+)
+
+arw_annotate <- arrow(length = unit(5, 'pt'), type = 'closed')
+p_inset <- tibble::tibble(
+  total_mp = c(2700),
+  n = 1:4
+) %>% 
+  base_plot(point.size = 3, segment.size = 2) +
+  scale_alpha_continuous(range = c(1, 1)) +
+  geom_fit_text(
+    inherit.aes = FALSE,
+    data = tibble(
+      xmin = c(100),
+      xmax = c(1200),
+      ymin = c(2),
+      ymax = c(3),
+      lab = 'more minutes'
+    ),
+    color = 'white',
+    aes(xmin = xmin, xmax = xmax, ymin = ymin, ymax = ymax, label = lab),
+    min.size = 0, grow = TRUE
+  ) +
+  geom_segment(
+    inherit.aes = FALSE,
+    aes(x = 1250, xend = 1500, y = 2.5, yend = 2.5),
+    linetype = 1,
+    color = 'white',
+    arrow = arw_annotate
+  ) +
+  geom_fit_text(
+  inherit.aes = FALSE,
+  data = tibble(
+    xmin = c(2700),
+    xmax = c(4000),
+    ymin = c(0.1),
+    ymax = c(3),
+    lab = 'more fouls'
+  ),
+  color = 'white',
+  aes(xmin = xmin, xmax = xmax, ymin = ymin, ymax = ymax, label = lab),
+  min.size = 0, grow = TRUE
+) +
+  geom_segment(
+    inherit.aes = FALSE,
+    aes(x = 3330, xend = 3330, y = 2, yend = 4),
+    linetype = 1,
+    color = 'white',
+    arrow = arw_annotate
+  ) +
+  theme(
+    plot.background = element_rect(color = 'white'),
+    axis.text = element_blank()
+  ) +
+  labs(x = NULL, y = NULL)
+p_inset
+
+path_inset <- file.path(dir_proj, 'rage_quit_insent.png')
+ggsave(
+  plot = p_inset,
+  filename = path_inset,
+  w = 8,
+  h = 8
+)
+
+path_w_inset <- add_logo(
+  path_viz = path_rage_quit,
+  path_logo = path_inset,
+  logo_scale = 0.25,
+  idx_x = 0.99,
+  idx_y = 0.01,
+  adjust_x = FALSE,
+  adjust_y = TRUE,
+  path_suffix = '_w_inset',
+  delete = FALSE
+)
+
+add_logo(
+  path_viz = path_w_inset,
+  path_logo = file.path(dir_proj, 'cancelo-eyes.png'),
+  logo_scale = 0.2,
+  idx_x = 0.01,
+  idx_y = 0.99,
+  adjust_x = TRUE,
+  adjust_y = FALSE,
+  path_suffix = '_and_cancelo',
+  delete = FALSE
+)
+
+# euro ---
+
+fouls <- all_actions %>% filter(type_name == 'foul')
+turnovers <- all_actions %>% filter(turnover)
+euro_fouls <- all_actions %>%
+  filter(
+    type_name == 'foul',
+    time_between_poss <= 5,
+    time_since_poss_start <= 3
+  )
+euro_fouls
 
 
 do_count_euro_fouls <- function(cols) {
