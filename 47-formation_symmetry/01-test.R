c61c1327345c1501428edd22df8d6d2817568f51
Tony ElHabr <anthonyelhabr@gmail.com>
2022-01-08 10:30:57 -0600

    formal test; trying formations

--- 47-formation_symmetry/01-test.R
+++ 47-formation_symmetry/01-test.R
@@ -1,56 +1,109 @@
 library(tidyverse)
 library(sf)
 
-df1 <- tibble::tribble(
-  ~node,  ~y,  ~x,
-    'b',   3, 1.5,
-    'c',   3, 8.5,
-    'd', 2.5,   3,
-    'e', 2.5,   7,
-    'f',   4,   2,
-    'g',   4,   8,
-    'h',   4,   4,
-    'i',   4,   6,
-    'j',   6, 3.5,
-    'k',   7,   6
+df <- tribble(
+  ~node,~y,  ~x,
+  'b',   3, 1.5,
+  'c',   3, 8.5,
+  'd', 2.5,   3,
+  'e', 2.5,   7,
+  'f',   4,   2,
+  'g',   4,   8,
+  'h',   4,   4,
+  'i',   4,   6,
+  'j',   6, 3.5,
+  'k',   7,   6
+)
+
+areas <- df %>% get_areas()
+areas
+
+areas$polygon_orig %>% plot(axes = TRUE, lwd = 1, lty = 2, border = 'orange')
+points(areas$data_orig[[1]]$x, areas$data_orig[[1]]$y, col = 'orange', pch = 1)
+points(areas$chull_orig[[1]]$x, areas$chull_orig[[1]]$y, col = 'orange', pch = 16)
+
+areas$polygon_flip %>% plot(add = TRUE, lwd = 1, lty = 2, border = 'blue')
+points(areas$data_flip[[1]]$x, areas$data_flip[[1]]$y, col = 'blue', pch = 1)
+points(areas$chull_flip[[1]]$x, areas$chull_flip[[1]]$y, col = 'blue', pch = 16)
+
+pi <- st_intersection(p1, p2)
+# pd <- st_difference(p1, p2)
+pd <- st_sym_difference(p1, p2)
+areas$polygon_inner %>% plot(add = TRUE, col = 'green')
+areas$polygon_outer %>% plot(add = TRUE, col = 'red')
+
+areas %>% 
+  transmute(
+    area_inner,
+    area_outer,
+    area_prop = area_inner / (area_inner + area_outer)
   )
 
-flip_df <- function(df, x_center = 5) {
-  df %>% 
-    mutate(
-      x = (2 * x_center) - x
+forms <- file.path('10-soccer_formations', 'formationListAndPlayerLocation.csv') %>% 
+  read_csv(
+    skip = 1L,
+    col_names = c('row', 'player', 'x', 'y', 'formation'),
+    col_types = cols(
+      row = col_double(),
+      player = col_integer(),
+      x = col_double(),
+      y = col_double(),
+      formation = col_character()
     )
-}
+  ) %>% 
+  select(-1) %>%
+  # Coercing to integer because join after voronoi calculation is inconsistent due to ".00" decimal places.
+  mutate(
+    across(c(x, y), ~ (.x * 100) %>% as.integer()),
+    across(formation, ~ str_replace_all(
+      .x,
+      c(
+        'one' = '1-',
+        'two' = '2-',
+        'three' = '3-',
+        'four' = '4-',
+        'five' = '5-'
+      )
+    ))
+  ) %>% 
+  mutate(across(formation, ~str_remove(.x, '-$'))) %>% 
+  mutate(idx_form = dense_rank(formation)) %>% 
+  arrange(idx_form)
+forms
+forms %>% count(x, y, sort = TRUE)
 
-get_ch <- function(df, col_x = 'x', col_y = 'y') {
-  ch_idx <- chull(df[[col_x]], df[[col_y]])
-  df[ch_idx, ]
-}
+formations <- forms %>% 
+  distinct(formation) %>% 
+  pull(formation)
+formations
 
-get_polygon <- function(df) {
-  df %>%
-    select(x, y) %>% 
-    add_row(head(., 1)) %>% 
-    as.matrix() %>% 
-    list() %>% 
-    st_polygon() %>% 
-    st_sfc()
-}
-
-ch1 <- df1 %>% get_ch()
-p1 <- ch1 %>% get_polygon()
-a1 <- p1 %>% st_area()
-
-df2 <- df1 %>% flip_df()
-ch2 <- df2 %>% get_ch()
-p2 <- ch2 %>% get_polygon()
-a2 <- p2 %>% st_area()
-
-p1 %>% plot(axes = TRUE)
-points(df1$x, df1$y, col = 'red')
-points(ch1$x, ch1$y, col = 'red')
-
-p2 %>% plot(add = TRUE)
-points(df2$x, df2$y, col = 'blue')
-points(ch2$x, ch2$y, col = 'blue')
-sf::st_overlaps(p1, p2)
+library(ggplot2)
+library(ggsoccer)
+p <- forms %>% 
+  filter(idx_form == 1) %>% 
+  ggplot() +
+  aes(x = x, y = y) +
+  ggsoccer::annotate_pitch(colour = 'black', fill = 'white') +
+  # geom_point(
+  #   shape = 21,
+  #   color = 'black',
+  #   size = 4,
+  #   alpha = 1
+  # ) +
+  geom_text(
+    aes(label = player),
+    size = 4
+  ) +
+  coord_flip(xlim = c(1, 99), ylim = c(4, 96), clip = 'on') +
+  theme(
+    axis.title = element_text(size = 12, hjust = 0),
+    axis.ticks = element_blank(),
+    axis.text = element_blank(),
+    panel.grid.major = element_blank(),
+    panel.grid.minor = element_blank()
+  ) +
+  labs(
+    y = NULL,
+    x = NULL
+  )
+p
---
+++ 47-formation_symmetry/helpers.R
@@ -0,0 +1,76 @@
+library(dplyr)
+library(tibble)
+library(sf)
+
+flip_df <- function(df, x_center = 5) {
+  df %>% 
+    mutate(
+      x = (2 * x_center) - x
+    )
+}
+
+.get_ch_idx <- function(df, col_x = 'x', col_y = 'y') {
+  chull(df[[col_x]], df[[col_y]])
+}
+
+get_ch <- function(df, ...) {
+  ch_idx <- .get_ch_idx(df, ...)
+  df[ch_idx, ]
+}
+
+get_inner_pts <- function(df, ...) {
+  ch_idx <- .get_ch_idx(df, ...)
+  df[-ch_idx, ]
+}
+
+get_polygon <- function(df) {
+  df %>%
+    select(x, y) %>% 
+    add_row(head(., 1)) %>% 
+    as.matrix() %>% 
+    list() %>% 
+    st_polygon() %>% 
+    st_sfc()
+}
+
+get_areas <- function(df) {
+  if(nrow(df) <= 1) {
+    return(tibble())
+  }
+  ch1 <- df %>% get_ch()
+  p1 <- ch1 %>% get_polygon()
+  a1 <- p1 %>% st_area()
+  
+  df2 <- df %>% flip_df()
+  ch2 <- df2 %>% get_ch()
+  p2 <- ch2 %>% get_polygon()
+  a2 <- p2 %>% st_area()
+  
+  pi <- st_intersection(p1, p2)
+  # pd <- st_difference(p1, p2)
+  pd <- st_sym_difference(p1, p2)
+  
+  ai <- st_area(pi)
+  ads <- st_area(pds)
+
+  res <- tibble(
+    area_inner = ai,
+    area_outer = ads,
+    data_orig = list(df),
+    data_flip = list(df2),
+    chull_orig = list(ch1),
+    chull_flip = list(ch2),
+    polygon_orig = p1,
+    polygon_flip = p2,
+    polygon_inner = pi,
+    polygon_outer = pd
+  )
+  ip <- df %>% get_inner_pts()
+  if(nrow(ip) <= 2) {
+    return(res)
+  }
+  bind_rows(
+    res,
+    get_areas(ip)
+  )
+}
