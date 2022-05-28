2e8f7c56737380df26944d1e955c11703b5ff941
Tony ElHabr <anthonyelhabr@gmail.com>
2022-05-28 06:29:16 -0500

    nice dawgs

--- 54-202122_ucl_final/01-scrape.R
+++ 54-202122_ucl_final/01-scrape.R
@@ -44,7 +44,7 @@ scrape_player <- function(url, stat_type, overwrite = FALSE) {
 
 scrape_player_profile <- function(url, overwrite = FALSE) {
   player <- url |> basename()
-  path <- file.path(dir_data, sprintf('%s-%s.rds', stat_type, player))
+  path <- file.path(dir_data, sprintf('profile-%s.rds', player))
   suffix <- glue::glue('for `player = "{player}"`.')
   if(file.exists(path) & !overwrite) {
     cli::cli_inform(glue::glue('{Sys.time()}: Returning early {suffix}'), sep = '\n')
@@ -57,16 +57,6 @@ scrape_player_profile <- function(url, overwrite = FALSE) {
 }
 
 possibly_scrape_player <- possibly(scrape_player, otherwise = tibble(), quiet = FALSE)
-poss <- setNames(
-  urls$url,
-  basename(urls$url)
-) |> 
-  map_dfr(
-    possibly_scrape_player,
-    stat_type = 'possession',
-    .id = 'player_name'
-  )
-
 misc <- setNames(
   urls$url,
   basename(urls$url)
@@ -77,75 +67,26 @@ misc <- setNames(
     .id = 'player_name'
   )
 
-pass <- setNames(
+possibly_scrape_player_profile <- possibly(scrape_player_profile, otherwise = tibble(), quiet = FALSE)
+profiles <- setNames(
   urls$url,
   basename(urls$url)
 ) |> 
   map_dfr(
-    possibly_scrape_player,
-    stat_type = 'passing',
+    possibly_scrape_player_profile,
     .id = 'player_name'
   )
 
-teams <- poss |> 
-  distinct(player_name, team_name = Squad) |> 
+teams <- misc |> 
+  distinct(player_name, player_url, team_name = Squad) |> 
   filter(team_name %in% c('Liverpool', 'Real Madrid')) |> 
   as_tibble()
-
-df <- inner_join(
-  poss %>% 
-    janitor::clean_names() |> 
-    filter(season == '2021-2022') |> 
-    group_by(player_name) |> 
-    summarize(
-      across(megs_dribbles, sum, na.rm = TRUE)
-    ) |> 
-    ungroup() |> 
-    rename(megs = megs_dribbles),
-  misc %>% 
-    janitor::clean_names() |> 
-    filter(season == '2021-2022') |> 
-    group_by(player_name) |> 
-    summarize(
-      across(c(mins_per_90, fls, crd_y, crd_r, x2crd_y), sum, na.rm = TRUE)
-    ) |> 
-    ungroup() |> 
-    transmute(
-      player_name,
-      m90 = mins_per_90,
-      fouls = fls,
-      cards = crd_y + crd_r + x2crd_y
-    ),
-  by = 'player_name'
-) |> 
-  inner_join(teams, by = 'player_name') |> 
-  relocate(team_name, .after = 'player_name') |> 
-  filter(
-    !(player_name %in% c('Alisson', 'Thibaut-Courtois'))
+teams
+profiles |> 
+  inner_join(
+    teams,
+    by = 'player_name'
   ) |> 
-  mutate(
-    megs_p90 = megs / m90,
-    fouls_per_card = fouls / ifelse(cards == 0, 1, cards),
-    fouls_p90 = fouls / m90
-  ) |> 
-  filter(m90 >= 15) |> 
-  mutate(
-    across(
-      c(megs_p90, fouls_p90),
-      list(prnk = percent_rank)
-    )
-  )
-df |> arrange(desc(megs_p90))
-df |> filter(m90 > 10) |> arrange(desc(megs_p90))
-df |> filter(m90 > 10) |> arrange(fouls_per_card)
-df |> 
-  ggplot() +
-  aes(
-    x = megs_p90_prnk,
-    y = fouls_per_card
-  ) +
-  geom_point() +
-  ggrepel::geom_text_repel(
-    aes(label = player_name)
-  )
-write_rds(path_data)
+  as_tibble() |> 
+  janitor::clean_names() |> 
+  write_rds(path_data)
---
+++ 54-202122_ucl_final/02-analyze.R
@@ -0,0 +1,149 @@
+library(tidyverse)
+library(scales)
+library(extrafont)
+library(ggtext)
+library(ggfx)
+dir_proj <- '54-202122_ucl_final'
+path_data <- file.path(dir_proj, 'data.rds')
+
+gray_wv <- rgb(24, 24, 24, maxColorValue = 255)
+gray_grid_wv <- rgb(64, 64, 64, maxColorValue = 255)
+
+extrafont::loadfonts(device = 'win', quiet = TRUE)
+theme_set(theme_minimal())
+theme_update(
+  text = element_text(family = 'Karla'),
+  title = element_text(size = 20, color = 'white'),
+  plot.title = ggtext::element_markdown(face = 'bold', size = 20, color = 'white'),
+  plot.title.position = 'plot',
+  plot.subtitle = ggtext::element_markdown(size = 16, color = '#f1f1f1'),
+  axis.text = element_text(color = 'white', size = 14),
+  axis.title = element_text(size = 14, color = 'white', face = 'bold', hjust = 0.99),
+  axis.line = element_blank(),
+  panel.grid.major = element_line(color = gray_grid_wv),
+  panel.grid.minor = element_line(color = gray_grid_wv),
+  panel.grid.minor.x = element_blank(),
+  panel.grid.minor.y = element_blank(),
+  plot.margin = margin(10, 10, 10, 10),
+  plot.background = element_rect(fill = gray_wv, color = gray_wv),
+  plot.caption = ggtext::element_markdown(color = 'white', hjust = 1, size = 10, face = 'italic'),
+  plot.caption.position = 'plot',
+  plot.tag = ggtext::element_markdown(size = 12, color = 'white', hjust = 0),
+  plot.tag.position = c(0.01, 0.02),
+  panel.background = element_rect(fill = gray_wv, color = gray_wv)
+)
+
+pcts <- path_data |> 
+  read_rds() |> 
+  filter(versus != 'Goalkeepers') |> 
+  filter(scouting_period == 'Last 365 Days') |> 
+  filter(based_on_minutes >= 1000) |> 
+  distinct(
+    player,
+    team_name,
+    pos = versus,
+    stat_group ,
+    statistic,
+    p = percentile,
+    mp = based_on_minutes
+  ) |> 
+  mutate(
+    group = case_when(
+      statistic %in% c('Progressive Passes', 'Progressive Passes Rec', 'Progressive Carries', 'npxG') ~ 'nice',
+      statistic %in% c('Pressures', 'Tackles', 'Interceptions') ~ 'dawg',
+      TRUE ~ NA_character_
+    )
+  ) |> 
+  drop_na(group)
+
+agg <- pcts |> 
+  mutate(
+    across(
+      p,
+      ~ifelse(
+        group == 'dawg',
+        abs(50 - .x),
+        .x
+      )
+    )
+  ) |> 
+  group_by(player, team_name, group) |> 
+  summarize(
+    across(mp, first),
+    across(p, sum)
+  ) |> 
+  ungroup() |> 
+  group_by(group, team_name) |> 
+  mutate(
+    across(p, ~scales::rescale(.x, to = c(-100, 100)))
+  ) |> 
+  ungroup() |> 
+  pivot_wider(
+    names_from = group,
+    values_from = p
+  )
+
+p <- agg |> 
+  ggplot() +
+  aes(x = nice, y = dawg) +
+  geom_vline(
+    color = gray_grid_wv,
+    linetype = 2,
+    aes(xintercept = 0)
+  ) +
+  geom_hline(
+    color = gray_grid_wv,
+    linetype = 2,
+    aes(yintercept = 0)
+  ) +
+  with_outer_glow(
+    geom_point(
+      show.legend = FALSE,
+      color = '#c8102E',
+      data = agg |> filter(team_name == 'Liverpool'),
+      aes(size = mp)
+    ),
+    colour = alpha('#c8102E', 0.5)
+  ) +
+  with_outer_glow(
+    geom_point(
+      show.legend = FALSE,
+      color = '#FEBE10',
+      data = agg |> filter(team_name == 'Real Madrid'),
+      aes(size = mp)
+    ),
+    colour = alpha('#FEBE10', 0.5)
+  ) +
+  scale_size(
+    range = c(1, 6)
+  ) +
+  ggrepel::geom_text_repel(
+    family = 'Karla',
+    size = 12 / .pt,
+    color = 'white',
+    aes(label = player)
+  ) +
+  labs(
+    x = "Ain't got that dawg in him",
+    y = 'That boy not nice'
+  ) +
+  scale_x_continuous(
+    sec.axis = dup_axis(name = 'Got that dawg in him')
+  ) +
+  scale_y_continuous(
+    sec.axis = dup_axis(name = 'That boy nice')
+  ) +
+  theme(
+    panel.grid.major = element_blank(),
+    axis.text = element_blank(),
+    plot.title = ggtext::element_markdown(vjust = 0.5, hjust = 0.5),
+    axis.title.x = element_text(vjust = 0.5, hjust = 0.5),
+    axis.title.y = element_text(vjust = 0.5, hjust = 0.5)
+  ) +
+  labs(
+    caption = '<br/>Niceness based on blend of npxG and progression stats, relative to position.<br/>Dawgness based on pressures, tackles, and interceptions, relative to position.<br/>Point size based on minutes played.',
+    tag = '**Viz**: Tony ElHabr<br/>**Data**: StatsBomb bia fbref'
+  )
+p
+
+ggsave(p, filename = file.path(dir_proj, 'nice_dawgs.png'), width = 8, height = 8)
