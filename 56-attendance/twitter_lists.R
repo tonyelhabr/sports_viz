84245a6c1432bb0a0a55cf9b2f396c800de91f48
Tony ElHabr <anthonyelhabr@gmail.com>
2022-06-24 07:56:52 -0500

    twitter list poc

--- 56-attendance/twitter_lists.R
+++ 56-attendance/twitter_lists.R
@@ -1,3 +1,20 @@
 
+library(rtweet)
+library(purrr)
+library(dplyr)
 
-mls_club_list <- '8929253'
+token <- create_token(
+  app =             Sys.getenv('TWITTER_APP'),
+  consumer_key =    Sys.getenv('TWITTER_CONSUMER_API_KEY'),
+  consumer_secret = Sys.getenv('TWITTER_CONSUMER_API_SECRET'),
+  access_token =    Sys.getenv('TWITTER_ACCESS_TOKEN'),
+  access_secret =   Sys.getenv('TWITTER_ACCESS_TOKEN_SECRET')
+)
+
+mls_club_list <- 8929253
+team_accounts <- list(
+  'mls_usl' = 1524400157474836481,
+  'epl' = 786067957373894656, 
+  'champ' = 786070708405272576
+) |> 
+  map_dfr(rtweet::lists_members, token = token, .id = 'league')
