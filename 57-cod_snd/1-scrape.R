f7c0c05d99d178948be925fd880e3a0b5cf3eec9
Tony ElHabr <anthonyelhabr@gmail.com>
2022-07-08 07:17:47 -0500

    split up cod

--- 57-cod_snd/1-scrape.R
+++ 57-cod_snd/1-scrape.R
@@ -1,51 +1,21 @@
 
+## Data from https://twitter.com/IOUTurtle
 library(tidyverse)
 library(googlesheets4)
 library(magrittr)
 library(qs)
-library(scales)
 
 dir_proj <- '57-cod_snd'
 dir_data <- file.path(dir_proj, 'data')
 dir.create(dir_data, showWarnings = FALSE)
 
-blackish_background <- '#1c1c1c'
-gray_points <- '#4d4d4d'
-gray_text <- '#999999'
-
-font <- 'Titillium Web' ## Karla
-extrafont::loadfonts(quiet = TRUE)
-theme_set(theme_minimal())
-theme_update(
-  text = element_text(family = font),
-  title = element_text(size = 20, color = 'white'),
-  plot.title = element_text(face = 'bold', size = 20, color = 'white'),
-  plot.title.position = 'plot',
-  plot.subtitle = element_text(size = 16, color = '#f1f1f1'),
-  axis.text = element_text(color = 'white', size = 14),
-  axis.title = element_text(size = 14, color = 'white', face = 'bold', hjust = 0.99),
-  axis.line = element_blank(),
-  panel.grid.major = element_line(color = gray_points),
-  panel.grid.minor = element_line(color = gray_points),
-  panel.grid.minor.x = element_blank(),
-  panel.grid.minor.y = element_blank(),
-  plot.margin = margin(10, 10, 10, 10),
-  plot.background = element_rect(fill = blackish_background, color = blackish_background),
-  plot.caption = element_text(size = 12, color = 'white', hjust = 1),
-  plot.caption.position = 'plot',
-  plot.tag = ggtext::element_markdown(size = 12, color = 'white', hjust = 0),
-  plot.tag.position = c(0.01, 0.01),
-  panel.background = element_rect(fill = blackish_background, color = blackish_background)
-)
-# update_geom_defaults('text', list(colour = 'white', font = font))
-
 sheets <- list(
   '2022' = crossing(
     s = c(1, 2, 3, 4),
-    matches = c(1, 2, 3, 4)
+    raw_matches = c(1, 2, 3, 4)
   ) |> 
     mutate(
-      label = sprintf('S%d %s', s, ifelse(matches == 4, 'Major', sprintf('Wk%d', matches)))
+      label = sprintf('S%d %s', s, ifelse(raw_matches == 4, 'Major', sprintf('Wk%d', raw_matches)))
     ) |> 
     pull(label) %>%
     .[1:14],
@@ -53,10 +23,10 @@ sheets <- list(
     'Champs', 
     crossing(
       s = c(1, 2, 3, 4, 5),
-      matches = c(1, 2, 3, 4)
+      raw_matches = c(1, 2, 3, 4)
     ) |> 
       mutate(
-        label = sprintf('S%d %s', s, ifelse(matches == 4, 'Major', sprintf('HS%d', matches)))
+        label = sprintf('S%d %s', s, ifelse(raw_matches == 4, 'Major', sprintf('HS%d', raw_matches)))
       ) |> 
       pull(label)
   ),
@@ -113,7 +83,7 @@ read_snd_sheet <- function(year, sheet, overwrite = FALSE) {
   
   col_types <- paste0(rep('c', times), collapse = '')
   
-  matches <- read_sheet(
+  raw_matches <- read_sheet(
     ss = ss,
     sheet = sheet,
     range = range,
@@ -121,9 +91,9 @@ read_snd_sheet <- function(year, sheet, overwrite = FALSE) {
     col_types = col_types
   )
   
-  matches$match <- rep(1:28, each = 5)
+  raw_matches$match <- rep(1:28, each = 5)
   
-  first_na_grp <- matches |> 
+  first_na_grp <- raw_matches |> 
     group_by(match) |> 
     filter(row_number() == 2) |> 
     ungroup() |> 
@@ -131,7 +101,7 @@ read_snd_sheet <- function(year, sheet, overwrite = FALSE) {
     pull(match) |> 
     min()
   
-  clean_matches <- matches |> 
+  matches <- raw_matches |> 
     filter(match < !!first_na_grp) |> 
     relocate(match) |> 
     group_by(match) |> 
@@ -139,7 +109,7 @@ read_snd_sheet <- function(year, sheet, overwrite = FALSE) {
     filter(rn %in% c(1L, 2L)) |> 
     ungroup()
   
-  long_matches <- clean_matches |> 
+  long_matches <- matches |> 
     pivot_longer(
       -c(rn:team)
     ) |> 
@@ -178,9 +148,9 @@ read_snd_sheet <- function(year, sheet, overwrite = FALSE) {
   res
 }
 
-matches <- sheets |> 
+raw_matches <- sheets |> 
   mutate(
-    data = map2(year, sheet, read_snd_sheet, overwrite = TRUE)
+    data = map2(year, sheet, read_snd_sheet, overwrite = FALSE)
   ) |> 
   unnest(data)
 
@@ -196,7 +166,7 @@ fixed_rounds <- tibble(
   map = c(rep('Arklov Peak', 4), rep('St. Petrograd', 2))
 )
 
-clean_matches <- matches |> 
+matches <- raw_matches |> 
   anti_join(
     fixed_rounds |> 
       distinct(year, sheet, match, team, round), 
@@ -217,7 +187,7 @@ clean_matches <- matches |>
   ) |> 
   arrange(event, match, round, team)
 
-records <- clean_matches |> 
+rounds <- matches |> 
   group_by(year, sheet, match, team) |> 
   mutate(
     cumu_w = cumsum(win_round),
@@ -229,13 +199,13 @@ records <- clean_matches |>
   ) |> 
   ungroup() |> 
   inner_join(
-    clean_matches |> 
+    matches |> 
       filter(round == 1L) |> 
       distinct(year, sheet, match, team, starts_as_offense = is_offense), 
     by = c('year', 'sheet', 'match', 'team')
   ) |> 
   inner_join(
-    clean_matches |> 
+    matches |> 
       group_by(year, sheet, match, team) |> 
       slice_max(round, n = 1, with_ties = FALSE) |> 
       ungroup() |> 
@@ -250,341 +220,5 @@ records <- clean_matches |>
       ), 
     by = c('year', 'sheet', 'match', 'team')
   )
-records
-
-summarize_records <- function(records, ...) {
-  records |> 
-    group_by(pre_cumu_w, pre_cumu_l, ...) |> 
-    summarize(
-      n = n(),
-      across(c(win_round, win_match), sum)
-    ) |> 
-    ungroup() |> 
-    mutate(
-      win_round_prop = win_round / n,
-      win_match_prop = win_match / n
-    )
-}
-
-common_heatmap_layers <- function(...) {
-  list(
-    ...,
-    guides(
-      fill = 'none'
-    ),
-    scale_x_continuous(
-      labels = 0:5,
-      breaks = seq(.5, 5.5, by = 1),
-      expand = c(0, 0)
-    ),
-    scale_y_continuous(
-      labels = 0:5,
-      breaks = seq(.5, 5.5, by = 1),
-      expand = c(0, 0),
-      sec.axis = sec_axis(
-        trans = I, 
-        name = ' ', 
-        breaks = seq(0.5, 5.5, by = 1), 
-        labels = rep('', 6)
-      )
-    ),
-    theme(
-      panel.grid.major = element_blank(),
-      axis.title = element_text(hjust = 0.5),
-      plot.title = ggtext::element_markdown(hjust = 0.5),
-      axis.text = element_text(size = 16, face = 'bold')
-    ),
-    labs(
-      caption = 'CDL SnD Major and Weekly Matches, 2020 - present',
-      tag = '**Viz**: @TonyElHabr | **Data**: @IOUTurtle',
-      x = "Offensive Team's # of Pre-Round Wins",
-      y = "Defensive Team's # of Pre-Round Wins"
-    )
-  )
-}
-
-## match win %, given round state ---
-## need to split this out by who is offense in order to not have a symmetric heatmap
-n_records <- records |> summarize_records()
-p_offensive_match_win_prop <- n_records |> 
-  ggplot() +
-  common_heatmap_layers() +
-  geom_rect(
-    color = blackish_background,
-    alpha = 0.8,
-    aes(
-      fill = win_match_prop,
-      xmin = pre_cumu_w, 
-      ymin = pre_cumu_l,
-      xmax = pre_cumu_w + 1,
-      ymax = pre_cumu_l + 1
-    )
-  ) +
-  geom_text(
-    family = font,
-    color = 'white',
-    size = 12 / .pt,
-    fontface = 'bold',
-    aes(
-      x = pre_cumu_w + 0.5, 
-      y = pre_cumu_l + 0.5,
-      label = sprintf(
-        '%s\n(%s/%s)', 
-        scales::percent(win_match_prop, accuracy = 1), 
-        scales::comma(win_match), scales::comma(n)
-      )
-    )
-  ) +
-  labs(
-    title = 'Offensive Match Win %'
-  )
-p_offensive_match_win_prop
-
-ggsave(
-  p_offensive_match_win_prop,
-  filename = file.path(dir_proj, 'offensive_match_win_prop.png'),
-  width = 8,
-  height = 8
-)
-
-## round win %, given round state ---
-n_records_side <- records |> summarize_records(is_offense)
-offense_round_win_prop <- n_records_side |> 
-  filter(is_offense) |> 
-  mutate(
-    diff_win_round_prop = win_round_prop - 0.5
-  )
-
-max_diff_win_round_prop <- max(abs(offense_round_win_prop$diff_win_round_prop))
-p_offensive_round_win_prop <- offense_round_win_prop |> 
-  ggplot() +
-  common_heatmap_layers() +
-  geom_rect(
-    color = blackish_background,
-    aes(
-      fill = win_round_prop,
-      xmin = pre_cumu_w, 
-      ymin = pre_cumu_l,
-      xmax = pre_cumu_w + 1,
-      ymax = pre_cumu_l + 1
-    )
-  ) +
-  geom_text(
-    family = font,
-    color = blackish_background,
-    size = 12 / .pt,
-    fontface = 'bold',
-    aes(
-      x = pre_cumu_w + 0.5, 
-      y = pre_cumu_l + 0.5,
-      label = sprintf('%s\n(%s/%s)', scales::percent(win_round_prop, accuracy = 1), scales::comma(win_round), scales::comma(n))
-    )
-  ) +
-  scale_fill_gradient2(
-    low = '#af8dc3',
-    high = '#7fbf7b',
-    mid = 'white',
-    midpoint = 0.5,
-    limits = c(0.5 - max_diff_win_round_prop, 0.5 + max_diff_win_round_prop)
-  ) +
-  labs(
-    title = 'Offensive Round Win %'
-  )
-p_offensive_round_win_prop
-
-ggsave(
-  p_offensive_round_win_prop,
-  filename = file.path(dir_proj, 'offensive_round_win_prop.png'),
-  width = 8,
-  height = 8
-)
-
-## actual match win % vs. expected match win %, given round state ---
-e_match_win_prop <- crossing(
-  pre_cumu_w = 0:5,
-  pre_cumu_l = 0:5
-) |> 
-  mutate(
-    n_remain_max = 11 - (pre_cumu_w + pre_cumu_l),
-    n_remain_w = 6 - pre_cumu_w,
-    e_win_match_prop = map2_dbl(n_remain_w, n_remain_max, ~sum(dbinom(..1:..2, ..2, 0.5)))
-  )
-
-e_offensive_match_win_prop <- offense_round_win_prop |> 
-  inner_join(
-    e_match_win_prop,
-    by = c('pre_cumu_w', 'pre_cumu_l')
-  ) |> 
-  mutate(
-    diff_win_match_prop = win_match_prop - e_win_match_prop
-  )
-
-max_diff_win_match_prop <- max(abs(e_offensive_match_win_prop$diff_win_match_prop))
-p_e_offensive_match_win_prop <- e_offensive_match_win_prop |> 
-  ggplot() +
-  common_heatmap_layers() +
-  geom_rect(
-    color = blackish_background,
-    alpha = 0.8,
-    aes(
-      fill = diff_win_match_prop,
-      xmin = pre_cumu_w, 
-      ymin = pre_cumu_l,
-      xmax = pre_cumu_w + 1,
-      ymax = pre_cumu_l + 1
-    )
-  ) +
-  geom_text(
-    family = font,
-    # color = 'white',
-    color = blackish_background,
-    size = 12 / .pt,
-    fontface = 'bold',
-    aes(
-      x = pre_cumu_w + 0.5, 
-      y = pre_cumu_l + 0.5,
-      label = sprintf('%+.1f%%\n(%s)', round(100 * diff_win_match_prop, 1), scales::comma(n))
-    )
-  ) +
-  scale_fill_gradient2(
-    low = '#d8b365',
-    high = '#5ab4ac',
-    # low = '#e9a3c9',
-    # high = '#a1d76a',
-    mid = 'white',
-    midpoint = 0,
-    limits = c(0 - max_diff_win_match_prop, 0 + max_diff_win_match_prop)
-  ) +
-  labs(
-    title = 'Actual Offensive Match Win % - Expected Offensive Match Win %'
-  )
-p_e_offensive_match_win_prop
-
-ggsave(
-  p_e_offensive_match_win_prop,
-  filename = file.path(dir_proj, 'e_offensive_match_win_prop.png'),
-  width = 8,
-  height = 8
-)
-
-## todo: streaks ----
-n_records_won_prior_round <- records |> summarize_records(won_prior_round)
-n_records_won_prior_round |> 
-  filter(!is.na(won_prior_round)) |> 
-  # filter(pre_cumu_w == pre_cumu_l) |> 
-  transmute(
-    pre_cumu_w,
-    pre_cumu_l,
-    across(won_prior_round, ~ifelse(.x, 'won_prior_round', 'lost_prior_round')),
-    win_round
-  ) |> 
-  pivot_wider(
-    names_from = won_prior_round,
-    values_from = win_round,
-    values_fill = 0L
-  ) |> 
-  mutate(
-    win_round = won_prior_round / (lost_prior_round + won_prior_round)
-  ) |> 
-  arrange(desc(win_round))
-
-records |> 
-  filter(!is.na(won_prior_round)) |> 
-  filter(win_match) |> 
-  # filter(n_rounds >= 10) |> 
-  filter(round == 11) |> 
-  count(n_rounds, win_match, win_round, won_prior_round, is_offense) |> 
-  group_by(n_rounds, won_prior_round) |> 
-  mutate(
-    prop = n / sum(n)
-  ) |> 
-  ungroup()
-
-## actual outcomes vs expected ----
-w_probs <- dbinom(6, 6:11, 0.5)
-l_probs <- dbinom(0:5, 6:11, 0.5)
-all_probs <- sum(w_probs, l_probs)
-match_round_probs <- tibble(
-  n_rounds = rep(6L:11L, times = 2),
-  win_match = rep(c(TRUE, FALSE), each = 6),
-  e_prop = c(w_probs, l_probs) / all_probs
-)
-
-records |> 
-  count(win_match, n_rounds, sort = TRUE) |> 
-  mutate(
-    prop = n / sum(n)
-  ) |> 
-  inner_join(
-    match_round_probs,
-    by = c('n_rounds', 'win_match')
-  ) |> 
-  mutate(
-    diff_prop = prop - e_prop
-  ) |> 
-  filter(win_match) |>
-  arrange(desc(abs(diff_prop)))
-
-records |> 
-  count(win_match, n_rounds, is_offense_last_round, sort = TRUE) |> 
-  mutate(
-    prop = n / sum(n)
-  ) |> 
-  inner_join(
-    match_round_probs |> mutate(across(e_prop, ~0.5 * .x)),
-    by = c('n_rounds', 'win_match')
-  ) |> 
-  mutate(
-    diff_prop = prop - e_prop
-  ) |> 
-  filter(win_match) |>
-  arrange(desc(abs(diff_prop)))
-
-## most frequent round playout ----
-round_seqs <- records |> 
-  transmute(year, sheet, match, team, round, across(win_round, as.integer)) |> 
-  pivot_wider(
-    names_from = round,
-    values_from = win_round,
-    names_prefix = 'r'
-  ) |> 
-  group_by(across(starts_with('r'))) |> 
-  count(sort = TRUE) |> 
-  ungroup()
-round_seqs
-total_matches <- sum(round_seqs$n)
-37 / total_matches
-dbinom(6, 6, 0.5)
-round_seqs |> 
-  filter(!is.na(r11)) |> 
-  select(r9:n) |> 
-  group_by(across(starts_with('r'))) |> 
-  summarize(
-    across(n, sum)
-  ) |> 
-  ungroup() |> 
-  filter(r9 == 1L) |> 
-  mutate(prop = n / sum(n))
-
-records |> 
-  filter(pre_cumu_w == 5, pre_cumu_l == 3) |> 
-  count(n_rounds, win_match) |> 
-  mutate(prop = n / sum(n))
-dbinom(3, 3, 0.5)
-
-records |> 
-  filter(pre_cumu_w == 5, pre_cumu_l == 3) |> 
-  count(n_rounds, win_match, is_offense) |> 
-  group_by(is_offense) |> 
-  mutate(prop = n / sum(n)) |> 
-  ungroup()
-dbinom(3, 3, 0.5)
-prop.test(
-  
-)
-
-records |> 
-  filter(pre_cumu_w == 5, pre_cumu_l == 4) |> 
-  count(n_rounds, win_match) |> 
-  mutate(prop = n / sum(n))
-dbinom(2, 2, 0.5)
+rounds
+qs::qsave(rounds, file.path(dir_proj, 'cod_rounds.qs'))
---
+++ 57-cod_snd/2-visualize.R
@@ -0,0 +1,441 @@
+
+library(tidyverse)
+library(qs)
+library(scales)
+library(extrafont)
+library(ggtext)
+
+blackish_background <- '#1c1c1c'
+gray_points <- '#4d4d4d'
+gray_text <- '#999999'
+
+font <- 'Titillium Web' ## Karla
+extrafont::loadfonts(quiet = TRUE)
+theme_set(theme_minimal())
+theme_update(
+  text = element_text(family = font),
+  title = element_text(size = 20, color = 'white'),
+  plot.title = element_text(face = 'bold', size = 20, color = 'white'),
+  plot.title.position = 'plot',
+  plot.subtitle = element_text(size = 16, color = '#f1f1f1'),
+  axis.text = element_text(color = 'white', size = 14),
+  axis.title = element_text(size = 14, color = 'white', face = 'bold', hjust = 0.99),
+  axis.line = element_blank(),
+  panel.grid.major = element_line(color = gray_points),
+  panel.grid.minor = element_line(color = gray_points),
+  panel.grid.minor.x = element_blank(),
+  panel.grid.minor.y = element_blank(),
+  plot.margin = margin(10, 10, 10, 10),
+  plot.background = element_rect(fill = blackish_background, color = blackish_background),
+  plot.caption = element_text(size = 12, color = 'white', hjust = 1),
+  plot.caption.position = 'plot',
+  plot.tag = ggtext::element_markdown(size = 12, color = 'white', hjust = 0),
+  plot.tag.position = c(0.01, 0.01),
+  panel.background = element_rect(fill = blackish_background, color = blackish_background)
+)
+# update_geom_defaults('text', list(colour = 'white', font = font))
+
+game_mapping <- c(
+  '2022' = 'Vanguard',
+  '2021' = 'Cold War',
+  '2020' = 'MW2 2020'
+)
+
+rounds <- qs::qread(file.path(dir_proj, 'cod_rounds.qs')) |> 
+  mutate(
+    game = game_mapping[as.character(year)],
+    .before = 1
+  )
+
+## offensive win % ----
+calculate_offensive_round_win_prop <- function(df) {
+  df |> 
+    count(year, game, map, is_offense, win_round) |> 
+    group_by(map, is_offense) |> 
+    mutate(total = sum(n), prop = n / total) |> 
+    ungroup() |>
+    filter(win_round, is_offense) |>
+    select(
+      year,
+      game,
+      map,
+      rounds_won = n,
+      total_rounds = total,
+      win_round_prop = prop
+    ) |>
+    arrange(desc(win_round_prop))
+}
+
+summarize_offensive_round_win_prop <- function(.round) {
+
+  specified_round <- rounds |> 
+    filter(round == .round) |> 
+    calculate_offensive_round_win_prop()
+
+  other_rounds <- rounds |> 
+    filter(round != .round) |> 
+    calculate_offensive_round_win_prop()
+  
+  post_rounds <- rounds |> 
+    filter(round > .round) |> 
+    calculate_offensive_round_win_prop()
+  
+  list(
+    specified_round,
+    other_rounds |> rename_with(~sprintf('other_%s', .x), -c(year, game, map)),
+    post_rounds |> rename_with(~sprintf('post_%s', .x), -c(year, game, map))
+  ) |> 
+    reduce(inner_join, by = c('year', 'game', 'map'))
+}
+offensive_win_r1_prop <- summarize_offensive_round_win_prop(1)
+offensive_win_r2_prop <- summarize_offensive_round_win_prop(2)
+devtools::source_url('https://raw.githubusercontent.com/dgrtwo/splittestr/master/R/vectorized-prop-test.R')
+# list(
+#   offensive_win_any_round_prop,
+#   offensive_win_r1_prop |> rename(n_r1 = n, prop_r1 = prop),
+#   offensive_win_r2_prop |> rename(n_r2 = n, prop_r2 = prop)
+# ) |> 
+#   reduce(inner_join) |> 
+#   mutate(
+#     prop_r1_diff = prop_r1 - prop,
+#     prop_r2_diff = prop_r2 - prop
+#   ) |> 
+#   arrange(desc(abs(prop_r1_diff)))
+
+
+## match win %, given round state ---
+summarize_rounds <- function(rounds, ...) {
+  rounds |> 
+    group_by(pre_cumu_w, pre_cumu_l, ...) |> 
+    summarize(
+      n = n(),
+      across(c(win_round, win_match), sum)
+    ) |> 
+    ungroup() |> 
+    mutate(
+      win_round_prop = win_round / n,
+      win_match_prop = win_match / n
+    )
+}
+
+common_heatmap_layers <- function(...) {
+  list(
+    ...,
+    guides(
+      fill = 'none'
+    ),
+    scale_x_continuous(
+      labels = 0:5,
+      breaks = seq(.5, 5.5, by = 1),
+      expand = c(0, 0)
+    ),
+    scale_y_continuous(
+      labels = 0:5,
+      breaks = seq(.5, 5.5, by = 1),
+      expand = c(0, 0),
+      sec.axis = sec_axis(
+        trans = I, 
+        name = ' ', 
+        breaks = seq(0.5, 5.5, by = 1), 
+        labels = rep('', 6)
+      )
+    ),
+    theme(
+      panel.grid.major = element_blank(),
+      axis.title = element_text(hjust = 0.5),
+      plot.title = ggtext::element_markdown(hjust = 0.5),
+      axis.text = element_text(size = 16, face = 'bold')
+    ),
+    labs(
+      caption = 'CDL SnD Major and Weekly raw_matches, 2020 - present',
+      tag = '**Viz**: @TonyElHabr | **Data**: @IOUTurtle',
+      x = "Offensive Team's # of Pre-Round Wins",
+      y = "Defensive Team's # of Pre-Round Wins"
+    )
+  )
+}
+
+## need to split this out by who is offense in order to not have a symmetric heatmap
+n_rounds <- rounds |> summarize_rounds()
+p_offensive_match_win_prop <- n_rounds |> 
+  ggplot() +
+  common_heatmap_layers() +
+  geom_rect(
+    color = blackish_background,
+    alpha = 0.8,
+    aes(
+      fill = win_match_prop,
+      xmin = pre_cumu_w, 
+      ymin = pre_cumu_l,
+      xmax = pre_cumu_w + 1,
+      ymax = pre_cumu_l + 1
+    )
+  ) +
+  geom_text(
+    family = font,
+    color = 'white',
+    size = 12 / .pt,
+    fontface = 'bold',
+    aes(
+      x = pre_cumu_w + 0.5, 
+      y = pre_cumu_l + 0.5,
+      label = sprintf(
+        '%s\n(%s/%s)', 
+        scales::percent(win_match_prop, accuracy = 1), 
+        scales::comma(win_match), scales::comma(n)
+      )
+    )
+  ) +
+  labs(
+    title = 'Offensive Match Win %'
+  )
+p_offensive_match_win_prop
+
+ggsave(
+  p_offensive_match_win_prop,
+  filename = file.path(dir_proj, 'offensive_match_win_prop.png'),
+  width = 8,
+  height = 8
+)
+
+## round win %, given round state ---
+n_rounds_side <- rounds |> summarize_rounds(is_offense)
+offense_round_win_prop <- n_rounds_side |> 
+  filter(is_offense) |> 
+  mutate(
+    diff_win_round_prop = win_round_prop - 0.5
+  )
+
+max_diff_win_round_prop <- max(abs(offense_round_win_prop$diff_win_round_prop))
+p_offensive_round_win_prop <- offense_round_win_prop |> 
+  ggplot() +
+  common_heatmap_layers() +
+  geom_rect(
+    color = blackish_background,
+    aes(
+      fill = win_round_prop,
+      xmin = pre_cumu_w, 
+      ymin = pre_cumu_l,
+      xmax = pre_cumu_w + 1,
+      ymax = pre_cumu_l + 1
+    )
+  ) +
+  geom_text(
+    family = font,
+    color = blackish_background,
+    size = 12 / .pt,
+    fontface = 'bold',
+    aes(
+      x = pre_cumu_w + 0.5, 
+      y = pre_cumu_l + 0.5,
+      label = sprintf('%s\n(%s/%s)', scales::percent(win_round_prop, accuracy = 1), scales::comma(win_round), scales::comma(n))
+    )
+  ) +
+  scale_fill_gradient2(
+    low = '#af8dc3',
+    high = '#7fbf7b',
+    mid = 'white',
+    midpoint = 0.5,
+    limits = c(0.5 - max_diff_win_round_prop, 0.5 + max_diff_win_round_prop)
+  ) +
+  labs(
+    title = 'Offensive Round Win %'
+  )
+p_offensive_round_win_prop
+
+ggsave(
+  p_offensive_round_win_prop,
+  filename = file.path(dir_proj, 'offensive_round_win_prop.png'),
+  width = 8,
+  height = 8
+)
+
+## actual match win % vs. expected match win %, given round state ---
+e_match_win_prop <- crossing(
+  pre_cumu_w = 0:5,
+  pre_cumu_l = 0:5
+) |> 
+  mutate(
+    n_remain_max = 11 - (pre_cumu_w + pre_cumu_l),
+    n_remain_w = 6 - pre_cumu_w,
+    e_win_match_prop = map2_dbl(n_remain_w, n_remain_max, ~sum(dbinom(..1:..2, ..2, 0.5)))
+  )
+
+e_offensive_match_win_prop <- offense_round_win_prop |> 
+  inner_join(
+    e_match_win_prop,
+    by = c('pre_cumu_w', 'pre_cumu_l')
+  ) |> 
+  mutate(
+    diff_win_match_prop = win_match_prop - e_win_match_prop
+  )
+
+max_diff_win_match_prop <- max(abs(e_offensive_match_win_prop$diff_win_match_prop))
+p_e_offensive_match_win_prop <- e_offensive_match_win_prop |> 
+  ggplot() +
+  common_heatmap_layers() +
+  geom_rect(
+    color = blackish_background,
+    alpha = 0.8,
+    aes(
+      fill = diff_win_match_prop,
+      xmin = pre_cumu_w, 
+      ymin = pre_cumu_l,
+      xmax = pre_cumu_w + 1,
+      ymax = pre_cumu_l + 1
+    )
+  ) +
+  geom_text(
+    family = font,
+    # color = 'white',
+    color = blackish_background,
+    size = 12 / .pt,
+    fontface = 'bold',
+    aes(
+      x = pre_cumu_w + 0.5, 
+      y = pre_cumu_l + 0.5,
+      label = sprintf('%+.1f%%\n(%s)', round(100 * diff_win_match_prop, 1), scales::comma(n))
+    )
+  ) +
+  scale_fill_gradient2(
+    low = '#d8b365',
+    high = '#5ab4ac',
+    # low = '#e9a3c9',
+    # high = '#a1d76a',
+    mid = 'white',
+    midpoint = 0,
+    limits = c(0 - max_diff_win_match_prop, 0 + max_diff_win_match_prop)
+  ) +
+  labs(
+    title = 'Actual Offensive Match Win % - Expected Offensive Match Win %'
+  )
+p_e_offensive_match_win_prop
+
+ggsave(
+  p_e_offensive_match_win_prop,
+  filename = file.path(dir_proj, 'e_offensive_match_win_prop.png'),
+  width = 8,
+  height = 8
+)
+
+## todo: streaks ----
+n_rounds_won_prior_round <- rounds |> summarize_rounds(won_prior_round)
+n_rounds_won_prior_round |> 
+  filter(!is.na(won_prior_round)) |> 
+  # filter(pre_cumu_w == pre_cumu_l) |> 
+  transmute(
+    pre_cumu_w,
+    pre_cumu_l,
+    across(won_prior_round, ~ifelse(.x, 'won_prior_round', 'lost_prior_round')),
+    win_round
+  ) |> 
+  pivot_wider(
+    names_from = won_prior_round,
+    values_from = win_round,
+    values_fill = 0L
+  ) |> 
+  mutate(
+    win_round = won_prior_round / (lost_prior_round + won_prior_round)
+  ) |> 
+  arrange(desc(win_round))
+
+rounds |> 
+  filter(!is.na(won_prior_round)) |> 
+  filter(win_match) |> 
+  # filter(n_rounds >= 10) |> 
+  filter(round == 11) |> 
+  count(n_rounds, win_match, win_round, won_prior_round, is_offense) |> 
+  group_by(n_rounds, won_prior_round) |> 
+  mutate(
+    prop = n / sum(n)
+  ) |> 
+  ungroup()
+
+## actual outcomes vs expected ----
+w_probs <- dbinom(6, 6:11, 0.5)
+l_probs <- dbinom(0:5, 6:11, 0.5)
+all_probs <- sum(w_probs, l_probs)
+match_round_probs <- tibble(
+  n_rounds = rep(6L:11L, times = 2),
+  win_match = rep(c(TRUE, FALSE), each = 6),
+  e_prop = c(w_probs, l_probs) / all_probs
+)
+
+rounds |> 
+  count(win_match, n_rounds, sort = TRUE) |> 
+  mutate(
+    prop = n / sum(n)
+  ) |> 
+  inner_join(
+    match_round_probs,
+    by = c('n_rounds', 'win_match')
+  ) |> 
+  mutate(
+    diff_prop = prop - e_prop
+  ) |> 
+  filter(win_match) |>
+  arrange(desc(abs(diff_prop)))
+
+rounds |> 
+  count(win_match, n_rounds, is_offense_last_round, sort = TRUE) |> 
+  mutate(
+    prop = n / sum(n)
+  ) |> 
+  inner_join(
+    match_round_probs |> mutate(across(e_prop, ~0.5 * .x)),
+    by = c('n_rounds', 'win_match')
+  ) |> 
+  mutate(
+    diff_prop = prop - e_prop
+  ) |> 
+  filter(win_match) |>
+  arrange(desc(abs(diff_prop)))
+
+## most frequent round playout ----
+round_seqs <- rounds |> 
+  transmute(year, sheet, match, team, round, across(win_round, as.integer)) |> 
+  pivot_wider(
+    names_from = round,
+    values_from = win_round,
+    names_prefix = 'r'
+  ) |> 
+  group_by(across(starts_with('r'))) |> 
+  count(sort = TRUE) |> 
+  ungroup()
+round_seqs
+total_matches <- sum(round_seqs$n)
+37 / total_matches
+dbinom(6, 6, 0.5)
+round_seqs |> 
+  filter(!is.na(r11)) |> 
+  select(r9:n) |> 
+  group_by(across(starts_with('r'))) |> 
+  summarize(
+    across(n, sum)
+  ) |> 
+  ungroup() |> 
+  filter(r9 == 1L) |> 
+  mutate(prop = n / sum(n))
+
+rounds |> 
+  filter(pre_cumu_w == 5, pre_cumu_l == 3) |> 
+  count(n_rounds, win_match) |> 
+  mutate(prop = n / sum(n))
+dbinom(3, 3, 0.5)
+
+rounds |> 
+  filter(pre_cumu_w == 5, pre_cumu_l == 3) |> 
+  count(n_rounds, win_match, is_offense) |> 
+  group_by(is_offense) |> 
+  mutate(prop = n / sum(n)) |> 
+  ungroup()
+dbinom(3, 3, 0.5)
+prop.test(
+  
+)
+
+rounds |> 
+  filter(pre_cumu_w == 5, pre_cumu_l == 4) |> 
+  count(n_rounds, win_match) |> 
+  mutate(prop = n / sum(n))
+dbinom(2, 2, 0.5)
