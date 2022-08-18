8caba2e833acb491a9cbb998773a9c6bc8be49e5
Tony ElHabr <anthonyelhabr@gmail.com>
2022-08-18 09:04:23 -0500

    re-arrange data, better plot

--- 58-loss_tweets/1-scrape_tweets.R
+++ 58-loss_tweets/1-scrape_tweets.R
@@ -1,23 +1,32 @@
-library(tidyverse)
+library(dplyr)
+library(worldfootballR)
+library(janitor)
+library(readr)
 library(lubridate)
 library(rtweet)
-library(xengagement)
-library(worldfootballR)
+library(qs)
 
 dir_proj <- '58-loss_tweets'
+dir_data <- file.path(dir_proj, 'data')
+dir.create(dir_data)
 matches <- worldfootballR::fb_team_match_results('https://fbref.com/en/squads/19538871/2021-2022/Manchester-United-Stats') |> 
   as_tibble() |> 
   janitor::clean_names()
+
+matches |>
+  select(date, venue, opponent, result) |> 
+  write_csv(file.path(dir_data, 'manu_matches.csv'))
+
 match_dates <- matches |> 
   filter(comp == 'Premier League') |> 
   select(date) |> 
   pull(date) |> 
   lubridate::date()
-match_dates
+
 first_match_date <- min(match_dates)
 
 token <- xengagement::get_twitter_token()
-
+## this doesn't work?!?
 # max_id <- NULL
 # last_date <- Sys.Date()
 # res <- list()
@@ -35,8 +44,9 @@ token <- xengagement::get_twitter_token()
 #   i <- i + 1
 # }
 tweets <- rtweet::get_timeline(user = 'ManUtd', n = 3200)
-qs::qsave(tweets, file.path(dir_proj, 'data', 'tweets.qs'))
+qs::qsave(tweets, file.path(dir_data, 'manu_tweets.qs'))
 
+## use this for as many tweets as possible, searching thru the data set manually
 matchday_tweets <- tweets |> 
   filter(!is_retweet) %>%
   filter(!is_quote) %>%
@@ -48,48 +58,5 @@ matchday_tweets <- tweets |>
     date %in% match_dates
   ) |> 
   arrange(desc(created_at))
-# from:ManUtd since:2022-02-20 until:2022-02-20
+# manual searching like from:ManUtd since:2022-02-20 until:2022-02-20
 
-tonythemes::theme_set_tony()
-manu_tweets <- file.path(dir_proj, 'statuses.csv') |> read_csv()
-outcome_lvls <- c('Win', 'Draw or Loss')
-player_counts <- manu_tweets |> 
-  mutate(
-    across(outcome, ~ifelse(.x != 'win', outcome_lvls[[2]], outcome_lvls[[1]]))
-  ) |> 
-  separate_rows(
-    players,
-    sep = ','
-  ) |> 
-  count(outcome, player = players, sort = TRUE) |> 
-  mutate(
-    across(outcome, ~ordered(.x, outcome_lvls)),
-    grp = tidytext::reorder_within(player, n, outcome)
-  )
-
-y_labels <- player_counts %>% distinct(grp, player)
-
-p <- player_counts |> 
-  ggplot() +
-  aes(x = n, y = grp) +
-  geom_col() +
-  scale_x_continuous(
-    labels = scales::number_format(accuracy = 1)
-  ) +
-  scale_y_discrete(name = '', labels = y_labels %>% select(grp, player) %>% deframe()) +
-  facet_wrap(~outcome, scales = 'free') +
-  theme(
-    panel.grid.major.y = element_blank()
-  ) +
-  labs(
-    title = 'Appearances in end-of-match @ManUtd score tweets',
-    y = NULL,
-    x = '# of matches'
-  )
-p
-ggsave(
-  p,
-  filename = file.path(dir_proj, 'manu_tweets.png'),
-  width = 10,
-  height = 8
-)
---
+++ 58-loss_tweets/2-plot-tweets.R
@@ -0,0 +1,112 @@
+library(dplyr)
+library(readr)
+library(tidyr)
+library(stringr)
+library(tidytext)
+library(glue)
+library(extrafont)
+library(ggplot2)
+library(ggtext)
+library(tibble)
+
+dir_proj <- '58-loss_tweets'
+dir_data <- file.path(dir_proj, 'data')
+
+blackish_background <- '#1c1c1c'
+gray_points <- '#4d4d4d'
+gray_text <- '#999999'
+
+font <- 'Titillium Web'
+extrafont::loadfonts(quiet = TRUE)
+theme_set(theme_minimal())
+theme_update(
+  text = element_text(family = font),
+  title = element_text(size = 20, color = 'white'),
+  plot.title = ggtext::element_markdown(face = 'bold', size = 20, color = 'white'),
+  plot.title.position = 'plot',
+  plot.subtitle = ggtext::element_markdown(size = 16, color = '#f1f1f1'),
+  axis.text = element_text(color = 'white', size = 14),
+  axis.title = element_text(size = 14, color = 'white', face = 'bold', hjust = 0.99),
+  axis.line = element_blank(),
+  panel.grid.major = element_line(color = gray_points),
+  panel.grid.minor = element_line(color = gray_points),
+  panel.grid.minor.x = element_blank(),
+  panel.grid.minor.y = element_blank(),
+  plot.margin = margin(10, 10, 10, 10),
+  plot.background = element_rect(fill = blackish_background, color = blackish_background),
+  plot.caption = ggtext::element_markdown(color = 'white', hjust = 1, size = 10, face = 'italic'),
+  plot.caption.position = 'plot',
+  plot.tag = ggtext::element_markdown(size = 12, color = 'white', hjust = 0),
+  plot.tag.position = c(0.01, 0.02),
+  panel.background = element_rect(fill = blackish_background, color = blackish_background)
+)
+
+manu_color <- '#DA291C'
+manu_tweets <- file.path(dir_proj, 'manu_tweets.csv') |> read_csv()
+outcome_lvls <- c('Win', 'Draw or Loss')
+init_player_counts <- manu_tweets |> 
+  mutate(
+    across(outcome, ~ifelse(.x != 'win', outcome_lvls[[2]], outcome_lvls[[1]]))
+  ) |> 
+  separate_rows(
+    players,
+    sep = ','
+  ) |> 
+  mutate(
+    across(players, ~str_remove(.x, '^.*\\s+'))
+  ) |> 
+  count(outcome, player = players, sort = TRUE)
+
+n_players_appearing_only_once <- init_player_counts |> 
+  filter(n == 1) |> 
+  group_by(outcome) |> 
+  summarize(across(n, sum))
+
+player_counts <- bind_rows(
+  init_player_counts |> 
+    filter(n != 1),
+  n_players_appearing_only_once |> 
+    transmute(
+      outcome, 
+      player = sprintf('%d players with 1', n),
+      n = 1
+    )
+  ) |> 
+  mutate(
+    across(outcome, ~ordered(.x, outcome_lvls)),
+    grp = tidytext::reorder_within(player, n, outcome)
+  )
+
+y_labels <- player_counts %>% distinct(grp, player)
+
+p <- player_counts |> 
+  ggplot() +
+  aes(x = n, y = grp) +
+  geom_col(
+    # alpha = 0.8,
+    fill = manu_color
+  ) +
+  scale_x_continuous(
+    labels = scales::number_format(accuracy = 1)
+  ) +
+  scale_y_discrete(name = '', labels = y_labels %>% select(grp, player) %>% deframe()) +
+  facet_wrap(~outcome, scales = 'free') +
+  theme(
+    strip.text = element_text(size = 14, color = 'white', face = 'bold', hjust = 0),
+    panel.grid.major.y = element_blank()
+  ) +
+  labs(
+    title = glue::glue('Appearances in end-of-match <span style="color:#FBE122">@</span><span style="color:{manu_color}">ManUtd</span> score tweets'),
+    subtitle = '2021/22 Premier League',
+    tag = '**Viz**: Tony ElHabr',
+    caption = '<br/>',
+    y = NULL,
+    x = '# of matches'
+  )
+p
+ggsave(
+  p,
+  filename = file.path(dir_proj, 'manu_tweets.png'),
+  width = 10,
+  height = 8
+)
