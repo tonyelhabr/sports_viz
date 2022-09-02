43b86f82339517304144d6bf231786c6464ddc42
Tony ElHabr <anthonyelhabr@gmail.com>
2022-09-02 08:20:18 -0500

    all the shots

--- 59-xg_xpoints/1-calculate.R
+++ 59-xg_xpoints/1-calculate.R
@@ -1,5 +1,6 @@
 library(worldfootballR)
 library(tidyverse)
+library(lubridate)
 
 dir_proj <- '59-xg_xpoints'
 team_mapping <- file.path(dir_proj, 'team_mapping.csv') |> read_csv()
@@ -29,75 +30,104 @@ compute_goal_prob <- function(df){
     mutate(cumprob = cumsum(value))
 }
 
-understat_match_shots <- understat_match_shots('https://understat.com/match/18220')
+understat_match_shots <- understat_match_shots('https://understat.com/match/18220') |> 
+  as_tibble() |> 
+  janitor::clean_names()
 fotmob_match_shots <- fotmob_get_match_details('3900946')
 
-sim_understat_xg <- understat_match_shots |> 
-  transmute(
-    match_id,
-    team = ifelse(home_away == 'h', home_team, away_team),
-    opponent = ifelse(home_away == 'h', away_team, home_team),
-    side = home_away,
-    make = xG, 
-    miss = 1 - xG
-  ) |>
-  # group_by(match_id, team, opponent, side) |>
-  nest(data = -c(match_id, team, opponent, side)) |>
-  # nest() |> 
-  mutate(
-    data = map(data, compute_goal_prob)
-  ) |> 
-  unnest(cols = c(data)) |> 
-  group_by(match_id, team, opponent, side) |>
-  mutate(g = row_number() - 1L) |>
-  ungroup() |> 
-  arrange(match_id, side, g)
-sim_understat_xg
+rename_teams <- function(df, src) {
+  team_src <- sprintf('team_%s', src)
+  df |> 
+    left_join(
+      team_mapping |> 
+        select(.data$team_538, .data[[team_src]]),
+      by = c('home_team' = team_src)
+    ) |> 
+    select(-.data$home_team) |> 
+    rename(home_team = .data$team_538) |> 
+    left_join(
+      team_mapping |> 
+        select(.data$team_538, .data[[team_src]]),
+      by = c('away_team' = team_src)
+    ) |> 
+    select(-.data$away_team) |> 
+    rename(away_team = .data$team_538) |> 
+    mutate(
+      team = ifelse(is_home, home_team, away_team),
+      opponent = ifelse(is_home, away_team, home_team)
+    ) |> 
+    select(-c(home_team, away_team)) 
+}
 
-sim_fotmob_xg <- fotmob_match_shots |> 
+postprocess_permuted_xg <- function(df) {
+  df |> 
+    nest(data = c(make, miss)) |>
+    mutate(
+      data = map(data, compute_goal_prob)
+    ) |> 
+    unnest(cols = c(data)) |> 
+    group_by(across(-c(value, cumprob))) |>
+    mutate(g = row_number() - 1L) |>
+    ungroup() |> 
+    arrange(match_id, is_home, g)
+}
+
+understat_permuted_xg <- understat_match_shots |>
   transmute(
     match_id,
-    # home_team,
-    # away_team,
-    team_id,
+    date = lubridate::date(date),
+    home_team,
+    away_team,
+    is_home = home_away == 'h',
+    make = x_g, 
+    miss = 1 - x_g
+  ) |>
+  rename_teams('understat') |> 
+  postprocess_permuted_xg()
+
+fotmob_permuted_xg <- fotmob_match_shots |> 
+  transmute(
+    match_id,
+    date = strptime(match_time_utc, "%a, %b %d, %Y, %H:%M UTC", tz = 'UTC') |> lubridate::date(),
+    home_team,
+    away_team,
+    is_home = team_id == home_team_id,
     make = expected_goals, 
     miss = 1 - make
   ) |>
-  # group_by(match_id, team_id) |>
-  nest(data = -c(match_id, team_id)) |>
-  mutate(
-    data = map(data, compute_goal_prob)
-  ) |> 
-  unnest(cols = c(data)) |> 
-  group_by(match_id, team_id) |>
-  mutate(g = row_number() - 1L) |>
-  ungroup() |> 
-  arrange(team_id, g)
-sim_fotmob_xg
+  rename_teams('fotmob') |>
+  postprocess_permuted_xg()
+
 
 ## everything ----
-fotmob_shots <- load_fotmob_match_details(league_id = 47)
-understat_shots <- load_understat_league_shots(league = 'EPL') |> as_tibble()
-fotmob_team_mapping <- bind_rows(
-  fotmob_shots |> 
-    distinct(league_id, team_id = home_team_id, team = home_team),
-  fotmob_shots |> 
-    distinct(league_id, team_id = away_team_id, team = away_team)
-) |> 
-  distinct(league_id, team_id, team)
-
-understat_teams <- bind_rows(
-  understat_shots |> 
-    distinct(league, team = home_team),
-  understat_shots |> 
-    distinct(league, team = away_team)
-) |> 
-  filter(!is.na(league)) |> 
-  distinct(league, team)
-
-
-understat_shots |> 
+all_fotmob_shots <- load_fotmob_match_details(league_id = 47)
+all_understat_shots <- load_understat_league_shots(league = 'EPL') |> 
+  as_tibble() |> 
   janitor::clean_names() |> 
-  group_by(season, match_id) |> 
-  nest() |> 
-  nest(data = c(match_id, data))
+  filter(season >= 2021)
+
+all_understat_permuted_xg <- all_understat_shots |>
+  transmute(
+    match_id,
+    date = lubridate::date(date),
+    home_team,
+    away_team,
+    is_home = coalesce(h_a == 'h', home_away == 'h'), ## home_away used in 2022 season
+    make = coalesce(xG, x_g), 
+    miss = 1 - make
+  ) |>
+  rename_teams('understat') |> 
+  postprocess_permuted_xg()
+
+all_fotmob_permuted_xg <- all_fotmob_shots |> 
+  transmute(
+    match_id,
+    date = strptime(match_time_utc, "%a, %b %d, %Y, %H:%M UTC", tz = 'UTC') |> lubridate::date(),
+    home_team,
+    away_team,
+    is_home = team_id == home_team_id,
+    make = expected_goals, 
+    miss = 1 - make
+  ) |>
+  rename_teams('fotmob') |>
+  postprocess_permuted_xg()
