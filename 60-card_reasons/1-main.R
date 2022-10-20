2f6f2e265f3eb277a7008e48577f8bdaaa63fb9f
Tony ElHabr <anthonyelhabr@gmail.com>
2022-10-19 21:55:43 -0500

    clean up 60

--- 60-card_reasons/1-main.R
+++ 60-card_reasons/1-main.R
@@ -3,6 +3,8 @@ library(lubridate)
 library(worldfootballR)
 library(jsonlite)
 library(janitor)
+library(gt)
+library(gtExtras)
 
 dir_proj <- '60-card_reasons'
 dir_data <- file.path(dir_proj, 'data')
@@ -41,8 +43,8 @@ possibly_read_json_gz <- possibly(read_json_gz, otherwise = list('Events' = tibb
 country_abbs <- c('ENG', 'FRA', 'GER', 'ITA', 'ESP', 'USA')
 league_id_mapping <- tibble(
   league_id = c(
-    c(47, 53, 54, 55, 87, 130),
-    c(48,110,146, 86,140,8972)
+    c(47,  53, 54,  55,  87, 130),
+    c(48, 110, 146, 86, 140, 8972)
   ),
   country = rep(country_abbs, 2),
   tier = c(rep('1st', length(country_abbs)), rep('2nd', length(country_abbs))),
@@ -114,14 +116,12 @@ scrape_tickers_for_league <- function(league_id, overwrite = FALSE) {
 }
 
 tickers <- league_id_mapping |> 
-  # slice(c(7)) |> 
   pull(league_id) |> 
   map_dfr(~scrape_tickers_for_league(.x, overwrite = FALSE)) |> 
   mutate(
     idx = row_number(),
     .before = 1
   )
-tickers
 
 card_incident_codes <- c('YC', 'RC', 'Y2C')
 card_events <- tickers |> 
@@ -138,9 +138,6 @@ words <- card_events |>
     stem = word |> SnowballC::wordStem()
   )
 
-word_counts <- words |> 
-  count(league_name, word, sort = TRUE)
-
 ns <- tickers |> 
   group_by(league_id) |> 
   summarize(
@@ -150,7 +147,6 @@ ns <- tickers |>
 
 .gt_theme_538 <- function(data, ...) {
   data |>
-    # gt::opt_all_caps() |> 
     gt::opt_table_font(
       font = list(
         gt::google_font('Karla'),
@@ -227,9 +223,6 @@ plot_frequency <- function(.stems, .label = .stems[1], title = NULL, subtitle = 
     ) |> 
     relocate(league_id, country, tier) |> 
     arrange(-interesting_cards_per_match)
-  
-  file_suffix <- glue::glue_collapse(.label, sep = '_')
-  .label <- glue::glue_collapse(.label, sep = '", "', last = '" or "')
   col_label <- glue::glue('With <b>"{.label}"</b><br/>in description')
   
   interesting_decimals <- ifelse(max(df$interesting_cards_per_match) > 1, 1, 2)
@@ -303,11 +296,6 @@ plot_frequency <- function(.stems, .label = .stems[1], title = NULL, subtitle = 
       columns = prop,
       decimals = prop_decimals
     ) |> 
-    # gt::cols_width(
-    #   # interesting_cards_per_match ~ px(150),
-    #   #cards_per_match ~ px(75),
-    #   prop ~ px(75)
-    # ) |> 
     gt::tab_source_note(
       source_note = gt::html('<i><b>Data</b>: Matches since beginning of 2020/21 seasons. (2021 seasons for U.S. leagues.)</i>')
     ) |> 
@@ -319,15 +307,12 @@ plot_frequency <- function(.stems, .label = .stems[1], title = NULL, subtitle = 
       columns = prop,
       palette = palette
     )
-  
-  n_row <- nrow(df)
-  # vwidth <- ifelse(n_row == 12, 480, 400)
-  
+
   gt::gtsave(
     tb,
     vwidth = 450,
     zoom = 2,
-    filename = file.path(dir_proj, sprintf('%s.png', file_suffix))
+    filename = file.path(dir_proj, sprintf('%s.png', .label))
   )
   invisible(tb)
 }
@@ -353,163 +338,3 @@ plot_frequency(
   title = 'EPL = premier drama',
   palette = 'ggsci::purple_material'
 )
-
-## new ----
-elapsed_times <- tickers |> 
-  group_by(league_id, match_id) |> 
-  summarize(
-    across(
-      c(elapsed, elapsed_plus), 
-      ~max(.x, na.rm = TRUE)
-    )
-  ) |> 
-  ungroup() |> 
-  mutate(
-    across(
-      elapsed_plus, 
-      ~case_when(
-        .x < 0L ~ 0L,
-        .x > 10L ~ 10L,
-        TRUE ~ .x
-      )
-    ),
-    total_elapsed = elapsed + elapsed_plus
-  ) |> 
-  filter(elapsed >= 90L)
-
-compute_card_per90_about_threshold <- function(threshold) {
-  # threshold <- 85L
-  elapsed_after <- elapsed_times |> 
-    mutate(diff = total_elapsed - !!threshold) |> 
-    group_by(league_id) |> 
-    summarize(
-      n_matches = n(),
-      across(diff, sum)
-    )
-  
-  cards_after <- card_events |> 
-    filter(elapsed >= !!threshold) |> 
-    count(league_id, league_name) |> 
-    inner_join(
-      elapsed_after, 
-      by = 'league_id'
-    ) |> 
-    mutate(
-      cards_per_min = n / diff
-    )
-  
-  elapsed_before <- elapsed_times |> 
-    mutate(diff = !!threshold - 1) |> 
-    group_by(league_id) |> 
-    summarize(
-      n_matches = n(),
-      across(diff, sum)
-    )
-  
-  cards_before <- card_events |> 
-    filter(elapsed < !!threshold) |> 
-    count(league_id, league_name) |> 
-    inner_join(
-      elapsed_before, 
-      by = 'league_id'
-    ) |> 
-    mutate(
-      cards_per_min = n / diff
-    )
-  
-  inner_join(
-    cards_before |> 
-      transmute(
-        league_id, 
-        league_name,
-        n_matches, 
-        n_cards_before = n, 
-        cards_per90_min_before = 90 * cards_per_min
-      ),
-    cards_after |> 
-      transmute(
-        league_id, 
-        n_cards_after = n, 
-        cards_per90_min_after = 90 * cards_per_min
-      ),
-    by = 'league_id'
-  ) |> 
-    mutate(
-      ratio = cards_per90_min_after / cards_per90_min_before
-    ) |> 
-    mutate(
-      rank = row_number(desc(ratio)),
-      .before = 1
-    ) |> 
-    arrange(desc(ratio))
-}
-
-card_ratios <- c(45, 60, 75, 80, 85, 86, 87, 88, 89, 90) |> 
-  map_dfr(
-    ~compute_card_per90_about_threshold(.x) |> 
-      mutate(threshold = .x, .before = 1)
-  )
-card_ratios |> filter(league_id == 8972)
-card_ratios |> filter(league_id == 47)
-
-card_elapsed_times <- card_events |> 
-  inner_join(
-    elapsed_times |> select(league_id, match_id, total_elapsed),
-    by = c('league_id', 'match_id')
-  ) |> 
-  filter(elapsed <= total_elapsed)
-
-card_times_by_league <- card_elapsed_times |> 
-  group_by(league_name) |> 
-  summarize(
-    across(
-      elapsed,
-      list(
-        median = median,
-        q75 = ~quantile(.x, 0.75),
-        q90 = ~quantile(.x, 0.9)
-      )
-    )
-  ) |> 
-  ungroup() |> 
-  arrange(desc(elapsed_q75))
-
-card_times_by_league |> 
-  pull(league_name)
-library(ggridges)
-card_elapsed_times |>
-  mutate(
-    across(league_name, ~ordered(.x, levels = rev(card_times_by_league$league_name)))
-  ) |> 
-  ggplot() +
-  aes(
-    x = elapsed, 
-    y = league_name, 
-    alpha = elapsed,
-    fill = stat(x),
-  ) +
-  geom_density_ridges_gradient(
-    scale = 1.5,
-    rel_min_height = 0.01,
-    quantiles = 4,
-    quantile_lines = TRUE
-  ) +
-  scale_fill_viridis_c(option = 'C', end = 1, begin = 0) +
-  coord_cartesian(
-    xlim = c(0, 100)
-  )
-usa_t2_league_id <- 8972
-matches_by_date <- load_fotmob_matches_by_date(league_id = usa_t2_league_id)
-match_ids <- matches_by_date |> 
-  filter(year(date) == 2022) |> 
-  distinct(match_id)
-matches_by_date |> filter(match_id == 3802716) |> glimpse()
-card_events |> 
-  semi_join(
-    match_ids,
-    by = 'match_id'
-  ) |> 
-  filter(league_id == usa_t2_league_id) |> 
-  count(match_id, sort = TRUE)
-card_events |> 
-  filter(match_id == 3802716)
