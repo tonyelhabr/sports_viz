08475e19ed46ec94ff699d7de2502ba3dcbc11cd
Tony ElHabr <anthonyelhabr@gmail.com>
2023-02-12 12:39:34 -0600

    more data pulling + gitignore

--- 65-opta_xg_calib/1-pull-footedness.R
+++ 65-opta_xg_calib/1-pull-footedness.R
@@ -1,50 +1,127 @@
-
-generate_league_url <- function(start_year) {
-  season_str <- sprintf('%04d-%04d', start_year, start_year + 1)
-  sprintf(
-    'https://fbref.com/en/comps/9/%s/stats/%s-Premier-League-Stats', season_str, season_str
-  )
-}
-
-league_urls <- c(
-  'https://fbref.com/en/comps/9/stats/Premier-League-Stats',
-  generate_league_url(c(2017:2021))
-)
-
 library(rvest)
 library(xml2)
 library(purrr)
-retrieve_league_players <- function(url) {
-  url <- 'https://fbref.com/en/comps/9/2021-2022/stats/2021-2022-Premier-League-Stats'
-  session <- chromote_session(url)
-  nodes <- session$find_nodes('table')
-  elements <- map_chr(nodes, function(node_id) {
-    json <- session$call_node_method(node_id)
-    json$result$value
-  })
-  html <- paste0('<html>', paste0(elements, collapse = '\n'), '</html>')
-  res <- xml_children(xml_children(read_html(html)))
-  session$html
-  res[[3]] |> 
-    html_elements('tbody > tr > td > a') |> 
-    html_attr('href')
+library(tibble)
+library(readr)
+library(dplyr)
+
+proj_dir <- '65-opta_xg_calib'
+data_dir <- file.path(proj_dir, 'data')
+footedness_data_dir <- file.path(data_dir, 'footedness')
+players_meta_data_dir <- file.path(data_dir, 'players', 'meta')
+dir.create(footedness_data_dir, recursive = TRUE, showWarnings = FALSE)
+dir.create(players_meta_data_dir, recursive = TRUE, showWarnings = FALSE)
+
+generate_league_url <- function(season_start_year) {
+  if (season_start_year == 2022) {
+    first_sep <- ''
+    second_sep <- ''
+    season_str <- ''
+  } else {
+    first_sep <- '/'
+    second_sep = '-'
+    season_str <- sprintf('%04d-%04d', season_start_year, season_start_year + 1)
+  }
+  
+  sprintf(
+    'https://fbref.com/en/comps/9/%s%sstats/%s%sPremier-League-Stats',
+    season_str, 
+    first_sep,
+    season_str,
+    second_sep
+  )
 }
 
-url <- 'https://fbref.com/en/comps/9/2021-2022/stats/2021-2022-Premier-League-Stats'
-worldfootballR:::.fb_single_league_stats
-session <- worldfootballR:::worldfootballr_chromote_session(url)
-nodes <- session$find_nodes("table")
-elements <- purrr::map_chr(nodes, function(node_id) {
-  json <- session$call_node_method(node_id)
-  json$result$value
-})
-html <- paste0("<html>", paste0(elements, collapse = "\n"), "</html>")
-res <- xml2::xml_children(xml2::xml_children(xml2::read_html(html)))
-# tables <- worldfootballR:::worldfootballr_html_table(session)
-# tables[[3]]
-res[[3]] |> rvest::html_elements("tbody > tr > td > a") |> rvest::html_attr('href')
-player_url <- 'https://fbref.com/en/players/d5dd5f1f/Pierre-Emerick-Aubameyang'
-page <- read_html(player_url)
-page |> 
-  html_elements(css = '#meta > div > p') |> 
-  html_text2()
+retrieve_league_players <- function(url) {
+  # url <- league_urls[1]
+  page <- read_html_live(url)
+  cells <- page |>
+    html_elements('tbody') |> 
+    pluck(3) |> 
+    html_elements('tr > td > a')
+  stats <- html_attr(cells, 'data-stat')
+  hrefs <- html_attr(cells, 'href')
+  text <- html_text2(cells)
+  stopifnot(length(hrefs) == length(text))
+  idx <- seq(1, length(cells), by = 4)
+  tibble(
+    player = text[idx],
+    team = text[idx + 2],
+    link = paste0('https://fbref.com/', hrefs[idx])
+  )
+}
+
+insistently_retrieve_league_players <- insistently(
+  retrieve_league_players, 
+  quiet = FALSE
+)
+
+retrieve_and_save_league_players <- function(season) {
+  url <- generate_league_url(season)
+  path <- file.path(footedness_data_dir, sprintf('ENG-1st-M-%s.rds', season))
+  if (file.exists(path)) {
+    message(sprintf('Reading in from "%s".', path))
+    return(read_rds(path))
+  }
+  message(sprintf('Collecting data at "%s".', url))
+  
+  res <- insistently_retrieve_league_players(url)
+  write_rds(res, path)
+  res
+}
+
+league_players <- 2017:2022 |> 
+  map_dfr(
+    ~retrieve_and_save_league_players(.x) |>
+      mutate(
+        season_start_year = as.integer(.x),
+        .before = 1
+      )
+  ) |> 
+  distinct(player_url = link)
+
+retrieve_player_meta <- function(url) {
+  page <- read_html(url)
+  page |> 
+    html_elements('#meta > div > p') |> 
+    html_text2()
+}
+
+retrieve_and_save_players_meta <- function(url) {
+  path <- file.path(players_meta_data_dir, paste0(basename(dirname(url)), '.txt'))
+  if (file.exists(path)) {
+    message(sprintf('Reading in from "%s".', path))
+    return(read_lines(path))
+  }
+  message(sprintf('Collecting data at "%s".', url))
+  Sys.sleep(3)
+  res <- retrieve_player_meta(url)
+  write_lines(res, path)
+  res
+}
+
+possibly_retrieve_and_save_players_meta <- possibly(
+ retrieve_and_save_players_meta,
+ otherwise = character(),
+ quiet = FALSE
+)
+
+players_meta <- league_players$player_url |> 
+  map_dfr(
+    ~{
+      res <- possibly_retrieve_and_save_players_meta(.x)
+      tibble(text = res) |> 
+        mutate(
+          player_url = .x,
+          .before = 1
+        )
+    }
+  )
+players_meta |>
+  filter(stringr::str_detect(text, '???'))
+  tidyr::separate(
+    text,
+    c('x1', 'x2', 'x3'),
+    sep = ':'
+  )
+  
