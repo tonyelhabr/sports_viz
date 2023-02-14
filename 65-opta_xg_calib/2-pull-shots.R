262b1e1cb17e4b1d2c65bbc69389d03efea073fd
Tony ElHabr <anthonyelhabr@gmail.com>
2023-02-14 05:47:42 -0600

    rm footedness in orig script

--- 65-opta_xg_calib/2-pull-shots.R
+++ 65-opta_xg_calib/2-pull-shots.R
@@ -1,14 +1,18 @@
-library(worldfootballR)
+library(readr)
 library(dplyr)
 library(lubridate)
 library(stringr)
 library(tidyr)
+library(probably)
+packageVersion('probably')
 
-match_shooting <- load_fb_match_shooting(
-  country = 'ENG', # c('ENG', 'ESP'),
-  tier = '1st',
-  gender = 'M'
-)
+proj_dir <- '65-opta_xg_calib'
+data_dir <- file.path(proj_dir, 'data')
+footedness_path <- file.path(data_dir, 'footedness.rds')
+match_shooting_path <- file.path(data_dir, 'match_shooting.rds')
+
+footedness <- read_rds(footedness_path)
+match_shooting <- read_rds(match_shooting_path)
 
 shots <- match_shooting |> 
   transmute(
@@ -24,53 +28,69 @@ shots <- match_shooting |>
     psxg = as.numeric(PSxG),
     outcome = Outcome,
     is_penalty = str_detect(Player, '\\(pen\\)'),
-    is_goal = outcome == 'Goal',
+    is_goal = factor(ifelse(outcome == 'Goal', 'yes', 'no')),
     distance = as.integer(Distance),
     body_part = `Body Part`,
+    notes = Notes,
     sca1 = Event_SCA_1,
     sca2 = Event_SCA_2
+  ) |> 
+  left_join(
+    footedness |> select(player, primary_foot = foot),
+    multiple = 'all',
+    by = join_by(player)
+  ) |> 
+  mutate(
+    is_true_open_play = notes == '',
+    is_from_deflection = str_detect(notes, 'Deflected'),
+    is_from_volley = str_detect(notes, 'Volley'),
+    is_free_kick = notes == 'Free kick',
+    is_primary_foot = case_when(
+      is.na(body_part) ~ NA,
+      !(body_part %in% sprintf('%s Foot', c('Left', 'Right'))) ~ NA,
+      primary_foot == tolower(str_remove(body_part, ' Foot')) ~ TRUE,
+      .default = FALSE
+    )
   )
 
 shots |> 
-  group_by(is_penalty) |> 
+  filter(!is_penalty) |> 
+  cal_plot_windowed(
+    truth = is_goal,
+    estimate = xg,
+    window_size = 0.05,
+    conf_level = 0.95,
+    event_level = 'second'
+  )
+
+shots |> 
+  filter(!is_penalty) |> 
+  cal_plot_breaks(
+    truth = is_goal,
+    estimate = xg,
+    group = is_primary_foot,
+    num_breaks = 20,
+    conf_level = 0.95,
+    event_level = 'second'
+  )
+  count(sca1) |> 
+  mutate(
+    is_open_play = case_when(
+      sca1 %in% c('Pass (Live)')
+    )
+  )
+
+shots |> 
+  filter(!is.na(is_primary_foot), !is_penalty) |> 
+  group_by(is_primary_foot) |> 
   summarize(
-    n = n(),
-    xg = sum(xg, na.rm = TRUE),
-    g = sum(is_goal)
+    n_shots = n(),
+    npxg = sum(xg, na.rm = TRUE),
+    npg = sum(as.character(is_goal) == 'yes')
   ) |> 
-  ungroup()
-
-n_shots_by_player <- shots |> count(team, player, sort = TRUE)
-
-footedness <- shots |> 
-  filter(body_part %in% sprintf('%s Foot', c('Left', 'Right'))) |> 
-  count(player, foot = tolower(str_remove(body_part, ' Foot'))) |> 
-  pivot_wider(
-    names_from = foot,
-    values_from = n,
-    values_fill = 0L
-  ) |> 
+  ungroup() |> 
   mutate(
-    total = left + right # ,
-    # left_prop = left / total,
-    # right_prop = right / total
-  ) |> 
-  ebbr::add_ebb_estimate(
-    x = right,
-    n = total,
-    prior_subset = total >= 20,
-    cred_level = 0.95
-  ) |> 
-  mutate(
-    foot = ifelse(.fitted >= 0.5, 'right', 'left'),
-    is_uncertain = .low < 0.5 & .high >= 0.5
-  ) |> 
-  arrange(desc(total))
-footedness |> filter(is_uncertain)
-footedness |> 
-  filter(.fitted < 0.5, total < 10) |> 
-  count(left, total, sort = TRUE)
-footedness |> 
-  filter(.fitted < 0.5, total == 10)
-footedness |> 
-  filter(.raw < 0.5, total >= 10, total <= 12)
+    d = npxg - npg
+  )
+
+
