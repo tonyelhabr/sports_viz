a21e6fe83612c73114af2d92223150e9849a7bab
Tony ElHabr <anthonyelhabr@gmail.com>
2023-04-02 17:27:14 -0500

    start 66

---
+++ 66-compare_club_rankings/1-plot.R
@@ -0,0 +1,145 @@
+library(readr)
+library(dplyr)
+library(gt)
+library(gtExtras)
+library(glue)
+
+proj_dir <- '66-compare_club_rankings'
+source(file.path(proj_dir, 'gt.R'))
+
+compared_rankings <- read_csv('https://github.com/tonyelhabr/club-rankings/releases/download/club-rankings/compared-rankings.csv') |> 
+  mutate(
+    drank = rank_538 - rank_opta
+  )
+
+compared_latest_rankings <- compared_rankings |> 
+  slice_max(date, n = 1, with_ties = TRUE)
+
+biggest_positive_top100_538_diffs <- compared_latest_rankings |> 
+  filter(
+    rank_538 <= 100
+  ) |> 
+  arrange(desc(drank)) |> 
+  slice_max(drank, n = 10, with_ties = FALSE)
+
+# biggest_negative_top100_538_diffs <- compared_latest_rankings |> 
+#   filter(
+#     rank_538 <= 100
+#   ) |> 
+#   arrange(drank) |> 
+#   slice_min(drank, n = 10, with_ties = FALSE)
+# 
+# biggest_positive_top100_opta_diffs <- compared_latest_rankings |> 
+#   filter(
+#     rank_opta <= 100
+#   ) |> 
+#   arrange(desc(drank)) |> 
+#   slice_max(drank, n = 10, with_ties = FALSE)
+# 
+# biggest_negative_top100_opta_diffs <- compared_latest_rankings |> 
+#   filter(
+#     rank_opta <= 100
+#   ) |> 
+#   arrange(drank) |> 
+#   slice_min(drank, n = 10, with_ties = FALSE)
+palette <- c(
+  '538' = '#ED713B',
+  'Opta' = '#7B1582' # '#00ADEF'
+)
+
+label_538 <- glue::glue('<b><span style="color:{palette[["538"]]}">538</span></b>')
+label_opta <- glue::glue('<b><span style="color:{palette[["Opta"]]}">Opta</span></b>')
+
+
+make_table <- function(
+    df, 
+    title, 
+    subtitle, 
+    filename = deparse(substitute(df)),
+    width,
+    rng_val,
+    vheight = 1100,
+    vwidth = vheight / 2
+  ) {
+  tb <- df |> 
+    transmute(
+      # league = gsub('^[A-z]+\\s', '', league_538),
+      league_538,
+      url_logo = sprintf('https://omo.akamai.opta.net/image.php?secure=true&h=omo.akamai.opta.net&sport=football&entity=team&description=badges&dimensions=150&id=%s', id_opta),
+      team_538,
+      # rank_538,
+      # rank_opta,
+      rank_538_dummy = rank_538,
+      rank_opta_dummy = rank_opta
+    ) |> 
+    gt() |> 
+    .gt_theme_538() |> 
+    gt::cols_label(
+      url_logo = '',
+      team_538 = '',
+      # rank_538 = '', # gt::html(glue::glue('<b><span style="color:{palette[["538"]]}">538</span></b>')),
+      # rank_opta = '', # gt::html(glue::glue('<b><span style="color:{palette[["Opta"]]}">Opta</span></b>')),
+      rank_538_dummy = '',
+      rank_opta_dummy = 'rank_opta_dummy'
+    ) |>
+    gtExtras::gt_merge_stack(team_538, league_538) |> 
+    gt::text_transform(
+      locations = gt::cells_body(columns = url_logo),
+      fn = function(x) {
+        gt::web_image(
+          url = x,
+          height = 25
+        )
+      }
+    ) |>
+    # gt::tab_spanner(
+    #   columns = c(rank_538, rank_opta),
+    #   label = 'Rank'
+    # ) |> 
+    gt_plt_dumbbell_custom(
+      rank_538_dummy,
+      rank_opta_dummy,
+      palette = c(unname(palette), '#D3D3D3'),
+      width = width,
+      rng_val = rng_val
+    ) |> 
+    gt::tab_header(
+      title = gt::md(glue::glue('**{title}**')),
+      subtitle = gt::md(subtitle)
+    ) |> 
+    gt::tab_source_note(
+      source_note = gt::md('***Updated**: 2023-04-07.*')
+    )
+  
+  gt::gtsave(
+    tb,
+    filename = file.path(proj_dir, sprintf('%s.png', filename)),
+    vheight = vheight,
+    vwidth = vwidth,
+    zoom = 2
+  )
+}
+
+make_table(
+  biggest_positive_top100_538_diffs,
+  title = 'Club ranking differences',
+  subtitle = glue::glue('Biggest deviations where {label_opta} rank is **lower** than {label_538}'),
+  width = 50,
+  rng_val = c(1, 100),
+  vheight = 1100,
+  vwidth = 550
+)
+
+compared_latest_rankings |> 
+  filter(
+    league_538 == 'Barclays Premier League'
+  ) |>
+  arrange(rank_538) |> 
+  make_table(
+    filename = 'epl',
+    title = 'EPL club rankings',
+    subtitle = glue::glue('Comparison of {label_538} and {label_opta} club rankings'),
+    width = 100,
+    rng_val = c(1, 150),
+    vheight = 1.5 * 1100
+  )
---
+++ 66-compare_club_rankings/gt.R
@@ -0,0 +1,201 @@
+library(rlang)
+library(magrittr)
+library(gtExtras)
+library(dplyr)
+library(ggplot2)
+library(scales)
+library(gt)
+
+## https://github.com/jthomasmock/gtExtras/blob/master/R/gt_plt_dumbbell.R
+gt_plt_dumbbell_custom <- function(
+    gt_object,
+    col1 = NULL,
+    col2 = NULL,
+    label = NULL,
+    palette = c("#378E38", "#A926B6", "#D3D3D3"),
+    width = 70,
+    text_args = list(accuracy = 1),
+    text_size = 3, # 2.5,
+    rng_val = NULL ## addition
+) {
+  stopifnot("'gt_object' must be a 'gt_tbl', have you accidentally passed raw data?" = "gt_tbl" %in% class(gt_object))
+  stopifnot("'palette' must be 3 colors in order of col1, col2, bar color" = length(palette) == 3)
+  
+  if (rlang::quo_is_null(rlang::enquo(col1)) | rlang::quo_is_null(rlang::enquo(col2))) {
+    stop("'col1' and 'col2' must be specified")
+  }
+  
+  # extract the values from specified columns
+  df_in <- gtExtras::gt_index(gt_object, {{ col1 }}, as_vector = FALSE) %>%
+    dplyr::select(x1 = {{ col1 }}, x2 = {{ col2 }})
+  
+  if (length(df) == 0) {
+    return(gt_object)
+  }
+  
+  all_vals <- df_in %>%
+    unlist() %>%
+    as.vector()
+  
+  if ((length(rng_val) != 2) | any(is.null(rng_val))) {
+    rng_val <- range(all_vals, na.rm = TRUE)
+  }
+
+  tab_out <- gt_object %>%
+    text_transform(
+      locations = cells_body({{ col1 }}),
+      fn = function(x) {
+        dumbbell_fx <- function(col1_vals, col2_vals, text_args, text_size) {
+          all_df_in_vals <- c(col1_vals, col2_vals)
+          
+          if (any(is.na(all_df_in_vals)) | any(is.null(all_df_in_vals))) {
+            return("<div></div>")
+          }
+
+          df_vals <- dplyr::tibble(x1 = col1_vals, x2 = col2_vals)
+          
+          # TODO: revisit horizontal adjustment
+          hjust_val <- ifelse(col1_vals >= col2_vals, list(1,0), list(0,1))
+
+          plot_obj <- ggplot(df_vals, aes(y = "y1")) +
+            geom_segment(
+              aes(x = x1, xend = x2, yend = "y1"),
+              linewidth = 1.5,
+              color = palette[3]
+            ) +
+            geom_point(
+              aes(x = x1),
+              color = "white",
+              pch = 21,
+              fill = palette[1],
+              size = 4, # 3,
+              stroke = 1.25
+            ) +
+            geom_point(
+              aes(x = x2),
+              color = "white",
+              pch = 21,
+              fill = palette[2],
+              size = 4, # 3,
+              stroke = 1.25
+            ) +
+            geom_text(
+              aes(
+                x = x1, y = 1.05,
+                label = do.call(scales::label_number, text_args)(x1),
+              ),
+              # TODO: revisit horizontal adjustment
+              # hjust = hjust_val[[1]],
+              family = 'Titillium Web', # "mono"
+              color = palette[1],
+              size = text_size,
+              
+            ) +
+            geom_text(
+              aes(
+                x = x2, y = 1.05,
+                label = do.call(scales::label_number, text_args)(x2),
+              ),
+              # TODO: revisit horizontal adjustment
+              # hjust = hjust_val[[2]],
+              family = "mono",
+              color = palette[2],
+              size = text_size
+            ) +
+            coord_cartesian(xlim = rng_val) +
+            # scale_x_continuous(expand = expansion(mult = c(0.1, 0.1))) +
+            scale_y_discrete(expand = expansion(mult = c(0.03, 0.095))) +
+            theme(
+              legend.position = "none",
+              plot.margin = margin(0, 0, 0, 0, "pt"),
+              plot.background = element_blank(),
+              panel.background = element_blank()
+            ) +
+            theme_void()
+          
+          gtExtras:::save_svg(plot_obj, height = 7, width = width, units = "mm")
+        }
+        
+        tab_built <- mapply(
+          dumbbell_fx,
+          df_in[[1]],
+          df_in[[2]],
+          list(text_args),
+          text_size,
+          SIMPLIFY = FALSE
+        )
+        tab_built
+      }
+    ) %>%
+    gt::cols_align(align = "left", columns = {{ col1 }}) %>%
+    gt::cols_hide({{ col2 }})
+  
+  if(!is.null(label)){
+    
+    return(
+      tab_out %>%
+        cols_label({{ col1 }} := label)
+    )
+  }
+  
+  tab_out
+  
+}
+
+.gt_theme_538 <- function(data, ...) {
+  data |>
+    opt_table_font(
+      font = list(
+        google_font('Titillium Web'),
+        default_fonts()
+      )
+    ) |>
+    tab_style(
+      style = cell_borders(
+        sides = "top", color = "black", weight = px(0)
+      ),
+      locations = gt::cells_column_labels(
+        columns = gt::everything()
+      )
+    ) |>
+    tab_style(
+      style = cell_borders(
+        sides = "bottom", color = "black", weight = px(1)
+      ),
+      locations = cells_row_groups()
+    ) |>
+    gt::tab_options(
+      column_labels.background.color = "white",
+      heading.border.bottom.style = "none",
+      table.border.top.width = px(3),
+      table.border.top.style = "none", # transparent
+      table.border.bottom.style = "none",
+      column_labels.font.weight = "normal",
+      column_labels.border.top.style = "none",
+      column_labels.border.bottom.width = gt::px(2),
+      column_labels.border.bottom.color = "black",
+      row_group.border.top.style = "none",
+      row_group.border.top.color = "black",
+      row_group.border.bottom.width = gt::px(1),
+      row_group.border.bottom.color = "white",
+      stub.border.color = "white",
+      stub.border.width = gt::px(0),
+      data_row.padding = gt::px(1),
+      source_notes.font.size = 12,
+      source_notes.border.lr.style = "none",
+      table.font.size = 14,
+      heading.title.font.size = 18,
+      heading.subtitle.font.size = 16,
+      heading.align = "left",
+      heading.padding = gt::px(0),
+      ...
+    ) |>
+    opt_css(
+      "tbody tr:last-child {
+        border-bottom: 2px solid #ffffff00;
+      }
+    ",
+    add = TRUE
+    )
+}
+
